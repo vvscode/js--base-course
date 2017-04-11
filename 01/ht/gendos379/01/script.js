bd533114c208d6da8a7a2e0b77a8a82b7a449234
Gennady Tsitovich <gendos20@yandex.ru>
2017-04-11 10:19:07 +0300

    Replaced tabs with spaces.

--- 01/ht/gendos379/01/script.js
+++ 01/ht/gendos379/01/script.js
@@ -8,15 +8,15 @@
  * @return {Number} - Result of calculation
  */
 function calc(firstOperand) {
-	return function(operator) {
-		return function(secondOperand) {
-			if (operations[operator]) {
-				return operations[operator](toNum(firstOperand), toNum(secondOperand));
-			} else {
-				console.log('Wrong operator');
-			}
-		}
-	}
+    return function(operator) {
+        return function(secondOperand) {
+            if (operations[operator]) {
+                return operations[operator](toNum(firstOperand), toNum(secondOperand));
+            } else {
+                console.log('Wrong operator');
+            }
+        }
+    }
 }
 
 /** Possible operations for calc function */
@@ -32,14 +32,14 @@ var operations = {
     },
     '/' : function(x, y) {
         return x / y;
-	}
+    }
 };
 
 /** Checks if given attribute is a number. */
 function toNum(num) {
-	if (isNaN(num)) {
-		console.log(num + ' is not a number!');
-	} else {
-		return +num;
-	}
+    if (isNaN(num)) {
+        console.log(num + ' is not a number!');
+    } else {
+        return +num;
+    }
 }
