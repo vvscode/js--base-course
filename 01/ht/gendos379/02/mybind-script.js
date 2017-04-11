c23309876714b52b931f38ec55038c7a24907911
Gennady Tsitovich <gendos20@yandex.ru>
2017-04-11 16:48:55 +0300

    Handling of additional attributes added

--- 01/ht/gendos379/02/mybind-script.js
+++ 01/ht/gendos379/02/mybind-script.js
@@ -2,9 +2,11 @@
  * Creates a new function that, when called, 
  * has its this keyword set to the provided value.
  */
-Function.prototype.myBind = function(scope) {
+Function.prototype.myBind = function() {
     var func = this;
+    var args = Array.prototype.slice.call(arguments);
+    var scope = args.shift();
     return function() {
-        return func.apply(scope);
+        return func.apply(scope, args.concat(Array.prototype.slice.call(arguments)));
     };
-}
+};
