da6a4063216df7ec812afb7239508b733722889c
Gennady Tsitovich <gendos20@yandex.ru>
2017-04-11 10:03:24 +0300

    Finalyzed refactored script.

--- 01/ht/gendos379/01/script.js
+++ 01/ht/gendos379/01/script.js
@@ -2,12 +2,25 @@
  * Makes calculations according to given attributes.
  * All attributes should be given in separated parenthesis, e.g.:
  * calc(3)('+')(2)
- * @param {Number} a - First number for calculation
- * @param {String} b - Arithmetic operator, possible values: '+' / '-' / '/' / '*'
- * @param {Number} c - Second number for calculation
+ * @param {Number} firstOperand - First number for calculation
+ * @param {String} operator - Arithmetic operator, possible values: '+' / '-' / '/' / '*'
+ * @param {Number} secondOperand - Second number for calculation
  * @return {Number} - Result of calculation
  */
-var operate = {
+function calc(firstOperand) {
+	return function(operator) {
+  		return function(secondOperand) {
+  			if (operations[operator]) {
+    			return operations[operator](toNum(firstOperand), toNum(secondOperand));
+    		} else {
+    			console.log('Wrong operator');
+    		}
+    	}
+  	}
+}
+
+/** Possible operations for calc function */
+var operations = {
     '+' : function(x, y) {
         return x + y;
     },
@@ -22,16 +35,11 @@ var operate = {
 	}
 };
 
-function calc(a) {
-  return operate[b](toNum(a), toNum(c); 
-}
-
 /** Checks if given attribute is a number. */
 function toNum(num) {
-  if (isNaN(num)) {	
-    console.log(num + ' is not a number!'); 
-  } else {
-    return +num;
-  }
+	if (isNaN(num)) {	
+    	console.log(num + ' is not a number!'); 
+  	} else {
+    	return +num;
+  	}
 }
-
