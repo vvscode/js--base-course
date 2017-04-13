1fe5dbb3207184eac4b9ac208a7ac7316af6352e
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2017-04-13 16:01:25 +0300

    Add gulp-rollup starter pack

---
+++ 02/lesson/src/main.js
@@ -0,0 +1,6 @@
+import greet from './utils/greet';
+
+const TEST_CONST = 3;
+
+greet(TEST_CONST);
+
---
+++ 02/lesson/src/utils/greet.js
@@ -0,0 +1,2 @@
+const TEST_CONST = 1;
+export default (param) => alert(`Hello! param=${param} TEST_CONST=${TEST_CONST}`);
