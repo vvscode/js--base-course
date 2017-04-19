12b8d480a78ed8276b9ffab055250f930c371ecb
Natasha <Selezen.natasha@gmail.com>
2017-04-19 20:09:00 +0300

    Update 02.js

--- 01/ht/NatashaSelezen/lesson 01/02.js
+++ 01/ht/NatashaSelezen/lesson 01/02.js
@@ -1,14 +1,14 @@
-var o = { name: 'O-object' };
- var f = function() {
- 	console.log(this.name);
- };
- Function.prototype.myBind = function() {
-   var fun = this;
-   var args = Array.prototype.slice.call(arguments);
-     return function() {
-        return fun.apply(context,BindArgs.contact(fnArgs));
-      };
- };
-var of = f.myBind(o);
+var o = { name: 'O-object' }; 
+var f = function() { 
+	console.log(this.name); 
+}; 
+Function.prototype.myBind = function(ctx) { 
+	var Args = Array.prototype.slice.call(arguments, 1) 
+	var cntx = this; 
+	return function() { 
+		cntx.apply(ctx, Args.concat(Array.prototype.slice.call(arguments)));
+	}; 
+}; 
+var of = f.myBind(o); 
 of();
    
