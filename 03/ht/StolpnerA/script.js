cf925b122f372d7306c05ec42444dddd437159d9
Andrey <andreysx@tut.by>
2017-06-06 14:07:26 +0300

    Edit

--- 03/ht/StolpnerA/script.js
+++ 03/ht/StolpnerA/script.js
@@ -13,23 +13,21 @@ function btnClick() {
 }
 
 function getLocationFetch(city) {
-    return new Promise((resolve, reject) => {
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${googleKey}`)
-            .then(response => response.json())
-            .then((data) => {
-                var lat = data.results[0].geometry.location['lat'];
-                var lng = data.results[0].geometry.location['lng'];
-                return fetch(`https://shrouded-spire-35703.herokuapp.com/forecast/${lat},${lng}?lang=ru&units=si`)
-            })
-            .then(response => response.json())
-            .then((data) => {
-                var tem = data.currently['temperature'];
-                var span = document.querySelector('.tem');
-                span.innerHTML = `${tem}&deg;C`;
-                addList(city);
-            })
-            .catch(reject => alert('лель'));
-    });
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${googleKey}`)
+        .then(response => response.json())
+        .then((data) => {
+            var lat = data.results[0].geometry.location['lat'];
+            var lng = data.results[0].geometry.location['lng'];
+            return fetch(`https://shrouded-spire-35703.herokuapp.com/forecast/${lat},${lng}?lang=ru&units=si`)
+        })
+        .then(response => response.json())
+        .then((data) => {
+            var tem = data.currently['temperature'];
+            var span = document.querySelector('.tem');
+            span.innerHTML = `${tem}&deg;C`;
+            addList(city);
+        })
+        .catch(reject => console.error(reject));
 }
 
 function getLocationXHR(city) {
@@ -64,5 +62,5 @@ function addList(city) {
     var ul = document.querySelector('.list');
     var li = document.createElement('li');
     li.innerHTML = city;
-    ul.appendChild(li) && arr.push(city);
+    ul.appendChild(li) && arr.unshift(city);
 }
