ff2d0cb42df07dbd0a1687281ff9d8104446a136
Andrey <andreysx@tut.by>
2017-06-07 11:31:55 +0300

    Edit SRP

--- 03/ht/StolpnerA/script.js
+++ 03/ht/StolpnerA/script.js
@@ -1,19 +1,49 @@
 const googleKey = `AIzaSyDa7DCL2NO9KMPd9DYVk_u3u0wCbm0XXFY`;
 var arr = new Array(4);
-var btn = document.querySelector('.search');
-btn.addEventListener('click', btnClick);
-
-function btnClick() {
+document.querySelector('form').addEventListener('submit', function (ev) {
+    ev.preventDefault();
     var city = document.querySelector('.searchLine');
     var fetch = document.querySelector('.fetch');
     var XHR = document.querySelector('.XHR');
     city = city.value;
-    if (fetch.checked) getLocationFetch(city);
-    else getLocationXHR(city);
-}
+    // window.location.hash = city;
+    var temperaturePromise = fetch.checked ? getTemperatureFetch(city) : getTemperatureXHR(city);
+    temperaturePromise
+        .then((data) => {
+            var tem = data.currently['temperature'];
+            var span = document.querySelector('.tem');
+            span.innerHTML = `${tem}&deg;C`;
+            addList(city);
+        });
+});
 
-function getLocationFetch(city) {
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${googleKey}`)
+
+// // Создать обработчик URL
+// function handleUrl(url) {
+//     var city = document.querySelector('.searchLine');
+//     var fetch = document.querySelector('.fetch');
+//     var XHR = document.querySelector('.XHR');
+//     city.value = url.slice(1);
+//     city = city.value;
+//     window.location.hash = city;
+//     if (fetch.checked) getTemperatureFetch(city)
+//         .then((data) => {
+//             var tem = data.currently['temperature'];
+//             var span = document.querySelector('.tem');
+//             span.innerHTML = `${tem}&deg;C`;
+//             addList(city);
+//         });
+//     else getTemperatureXHR(city);
+// }
+//
+// // // Подписаться на изменения URL
+// // window.addEventListener('hashchange', (ev) => handleUrl(ev.newURL));
+//
+// // При загрузке страницы - считать состояние и запустить обработчик
+// handleUrl(window.location.hash);
+
+function getTemperatureFetch(city) {
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${googleKey}`)
         .then(response => response.json())
         .then((data) => {
             var lat = data.results[0].geometry.location['lat'];
@@ -21,41 +51,39 @@ function getLocationFetch(city) {
             return fetch(`https://shrouded-spire-35703.herokuapp.com/forecast/${lat},${lng}?lang=ru&units=si`)
         })
         .then(response => response.json())
-        .then((data) => {
-            var tem = data.currently['temperature'];
-            var span = document.querySelector('.tem');
-            span.innerHTML = `${tem}&deg;C`;
-            addList(city);
-        })
         .catch(reject => console.error(reject));
 }
 
-function getLocationXHR(city) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${googleKey}`, true);
-    xhr.send();
-    xhr.onload = xhr.onerror = function () {
-        if (this.status == 200) {
+function getTemperatureXHR(city) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${googleKey}`, true);
+        xhr.send();
+
+        xhr.onload = xhr.onerror = function () {
+            if (this.status != 200) console.log('error:  ' + this.status);
             var data = this.response;
-            if (JSON.parse(data).status == "ZERO_RESULTS") {
-                console.log('ZERO_RESULTS');
-            }
-            else {
-                var lat = JSON.parse(data).results[0].geometry.location['lat'];
-                var lng = JSON.parse(data).results[0].geometry.location['lng'];
-                var xhr1 = new XMLHttpRequest();
-                xhr1.open('GET', `https://shrouded-spire-35703.herokuapp.com/forecast/${lat},${lng}?lang=ru&units=si`, true);
-                xhr1.send();
-                xhr1.onload = function () {
-                    let data = this.response;
-                    var tem = JSON.parse(data).currently['temperature'];
-                    var span = document.querySelector('.tem');
-                    span.innerHTML = `${tem}&deg;C`;
+            var lat = JSON.parse(data).results[0].geometry.location['lat'];
+            var lng = JSON.parse(data).results[0].geometry.location['lng'];
+            var xhr1 = new XMLHttpRequest();
+            xhr1.open('GET', `https://shrouded-spire-35703.herokuapp.com/forecast/${lat},${lng}?lang=ru&units=si`, true);
+            xhr1.send();
+            xhr1.onload = function () {
+                if (this.status == 200) {
+                    var data = this.response;
+                    resolve(JSON.parse(data));
                 }
-            }
+                else {
+                    var error = new Error(this.statusText);
+                    error.code = this.status;
+                    reject(error);
+                }
+            };
+            xhr1.onerror = function () {
+                reject(new Error('Network Error'))
+            };
         }
-        else console.log('error:  ' + this.status);
-    }
+    });
 }
 
 function addList(city) {
