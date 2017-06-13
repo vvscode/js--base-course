f5a78260de62f1b00dd77ff0b9d3c9f761479a77
Andrey <andreysx@tut.by>
2017-06-13 21:26:31 +0300

    Update 04 task

---
+++ 04/ht/StolpnerA/src/script/utils/eventBus.js
@@ -0,0 +1,43 @@
+/*
+ d1. Реализовать EventBus
+
+ создать конструктор
+ создать метод on
+ coздать метод off
+ создать метод trigger
+ создать метод once
+ вызвать конструктор
+ */
+
+function EventBus() {
+    this.listeners = {};
+}
+
+EventBus.prototype.on = function (ev, handler) {
+    this.listeners[ev] = this.listeners[ev] || [];
+    this.listeners[ev].push(handler);
+};
+EventBus.prototype.trigger = function (ev, data, a, b, c) {
+    (this.listeners[ev] || []).forEach((handler) => handler(data, a, b, c));
+};
+EventBus.prototype.off = function (ev, handler) {
+    (this.listeners[ev] || []).forEach(function (item, i) {
+        if (item === handler) {
+            this.listeners[ev].splice(i, 1);
+        }
+    }, this);
+};
+EventBus.prototype.once = function (ev, handler) {
+    var that = this;
+    this.on(ev, function offMethod() {
+        handler();
+        that.off(ev, offMethod);
+    });
+};
+
+var eventBus = new EventBus();
+var handler = (a, b, c) => console.log('Handler', a, b, c);
+eventBus.once('one', handler);
+eventBus.trigger('one', 1,2,3);
+// Handler 1 2 3
+eventBus.trigger('one');
---
+++ 04/ht/StolpnerA/src/script/utils/router.js
@@ -0,0 +1,3 @@
+/**
+ * Created by andre on 13-Jun-17.
+ */
