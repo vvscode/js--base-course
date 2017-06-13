ca57902d399c9dddb1fb07ad2e5b3749cee6777a
Andrey <andreysx@tut.by>
2017-06-13 23:55:33 +0300

    add eventBust and Edite

--- 04/ht/StolpnerA/src/script/utils/eventBus.js
+++ 04/ht/StolpnerA/src/script/utils/eventBus.js
@@ -17,8 +17,8 @@ EventBus.prototype.on = function (ev, handler) {
     this.listeners[ev] = this.listeners[ev] || [];
     this.listeners[ev].push(handler);
 };
-EventBus.prototype.trigger = function (ev) {
-    (this.listeners[ev] || []).forEach((handler) => handler());
+EventBus.prototype.trigger = function (ev, ...args) {
+    (this.listeners[ev] || []).forEach((handler) => handler.apply(null, args));
 };
 EventBus.prototype.off = function (ev, handler) {
     (this.listeners[ev] || []).forEach(function (item, i) {
@@ -30,7 +30,7 @@ EventBus.prototype.off = function (ev, handler) {
 EventBus.prototype.once = function (ev, handler) {
     var that = this;
     this.on(ev, function offMethod() {
-        handler();
+        handler.apply(null, arguments);
         that.off(ev, offMethod);
     });
 };
