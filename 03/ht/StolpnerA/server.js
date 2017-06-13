78e9f32c97429fc64608cc550e6e6d3d428921e7
Andrey <andreysx@tut.by>
2017-06-13 10:46:36 +0300

    Update 04 task

---
+++ 03/ht/StolpnerA/server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+
+http.createServer(function(req, res) {
+    file.serve(req, res);
+}).listen(8080);
+
+console.log('Server running on port 8080');
