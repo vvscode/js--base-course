547ed1687789a20d5f73bbea8c0e9fd11b477004
Sergei Pleshkov <azrailomgpro@gmail.com>
2017-10-04 14:03:33 +0300

    Partialy solved, still working on it

---
+++ 02/ht/SergeiPleshkov/script.js
@@ -0,0 +1,201 @@
+/* eslint no-var: "off" */
+/* eslint no-unused-vars: "off" */
+/* eslint max-len: "off" */
+
+/**
+ * Написать функцию `isDeepEqual`
+ * которая принимает на вход двe переменных
+ * и проверяет идентичны ли они по содержимому. Например
+ * @param {*} objA 
+ * @param {*} objB 
+ * @return {boolean} идентичны ли параметры по содержимому
+ */
+function isDeepEqual(objA, objB) {
+  if (objA === objB) {
+    return true;
+  } else if (JSON.stringify(objA) === JSON.stringify(objB)) {
+    return true;
+  } else if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  for (var key in objA) {
+    if (typeof objA[key] === 'object' && typeof objB[key] === 'object') {
+      return isDeepEqual(objA[key], objB[key]);
+    }
+  }
+  return false;
+}
+
+/**
+ * Функция фиксации контекста
+ * @param {*} func Функция для которой нужно зафиксировать контекст
+ * @param {*} context значение для this
+ * @return {function} функция с зафиксированным контекстом
+ */
+function bind(func, context) {
+  var bindArguments = [].slice.call(arguments, 2);
+  return function () {
+    var funcArguments = [].slice.call(arguments);
+    return func.apply(context, bindArguments.concat(funcArguments));
+  };
+}
+
+/**
+ * Реализовать метод .myBind для всех функций, 
+ * который работает так же как оригинальный .bind но не использует его внутри
+ * (можно использовать фукнцию выше)
+ */
+Function.prototype.myBind = function (oThis) {
+
+  var aArgs = Array.prototype.slice.call(arguments, 1),
+    fToBind = this,
+    fNOP = function () {},
+    fBound = function () {
+      return fToBind.apply(this instanceof fNOP && oThis ?
+        this :
+        oThis,
+        aArgs.concat(Array.prototype.slice.call(arguments)));
+    };
+
+  fNOP.prototype = this.prototype;
+  fBound.prototype = new fNOP();
+
+  return fBound;
+};
+/**
+ * Создать объект o так, чтобы каждый раз когда в коде написано 
+ * o.magicProperty = 3 // (любое значение) 
+ * в консоль выводилось значение, которое присваивается и текущее время
+ */
+
+/**
+ * Создать конструктор с методами, так, 
+ * чтобы следующий код работал и делал соответствующие вещи
+ * те запуск кода ниже должен делать то, что говорят методы
+ * u.askName().askAge().showAgeInConsole().showNameInAlert();
+ */
+
+/**
+ * Написать фукнцию-калькулятор, которая работает следующим образом
+ * calculate('+')(1)(2); // 3
+ * calculate('*')(2)(3); // 6
+ * Допустимые операции : + - * /
+ */
+function calculate() {
+  var operand = [].slice.call(arguments, 0);
+  return function (a) {
+
+    return function (b) {
+      return eval(a + operand + b);
+    };
+
+  }
+}
+
+/**
+ * Создайте конструктор-синглтон? Что такое синглтон?
+ * new Singleton() === new Singleton
+ */
+function Singleton() {
+  if (Singleton.instance) {
+    return Singleton.instance
+  }
+  Singleton.instance = this;
+}
+
+/**
+ * Создайте функцию ForceConstructor
+ * которая работает как конструктор независимо от того,
+ * вызвана она с new или без
+ * и сохраняет параметры в создаваемый объект с именами параметров
+ */
+function ForceConstructor(a, b, c) {
+  if (this instanceof ForceConstructor()) {
+    Object.defineProperties(this, {
+      'a': {value: a},
+      'b': {value: b},
+      'c': {value: c}})
+  } else {
+    return new ForceConstructor(a, b, c);
+  }
+}
+
+/**
+ * Написать фукнцию сумматор, которая будет работать 
+ * var s = sum();
+ * log(s); // 0
+ * log(s(1)); // 1
+ * log(s(1)(2)); //3
+ * log(s(3)(4)(5)); // 12
+ * Число вызовов может быть неограниченым
+ */
+
+function sum() {
+    var a = +[].slice.call(arguments, 0);
+    var currentSum = a;
+
+    function func(b) {
+        if (b) {
+            currentSum += b;
+            return func;
+        } else return func;
+    }
+
+    func.toString = function () {
+        var result = currentSum;
+        if (currentSum !== a) {
+            currentSum = a;
+        }
+        return result;
+    };
+
+    return func;
+}
+
+/**
+ * Написать каррирующую функцию и покрыть ее тестами
+ * Функция должна поддерживать каррирование функций с 2,3,4,5 параметрами
+ * пример работы  функции
+ * 
+ * function target1(a,b,c,d) { return a + b + c + d }
+ * function target2(a,b) { return a + b }
+ * curry(target1)(1)(2)(3)(4) // 10
+ * curry(target2)(5)(8) // 13
+ * 
+ * Примеры тестов смотреть в файле тестов
+ * 
+ * Читать
+ * http://prgssr.ru/development/vvedenie-v-karrirovanie-v-javascript.html
+ * @param {*} func 
+ */
+function curry(func) {}
+
+/*
+Написать код, который для объекта созданного с помощью конструктора будет показывать, 
+что объект является экземпляром двух классов
+*/
+/* Тут ваш код */
+// User === PreUser; // false
+// u instanceof User; // true
+// u instanceof Array; // true
+// u instanceof PreUser; // true
+
+/*
+Создать веб страницу. Добавить на нее форму с полями 
+- имя (строкое поле), 
+- родной город (Выпадающий список), 
+- Комментарий (многострочное поле), пол (radiobutton). 
+При нажатии на кнопку - нужно собрать данные введенные в поля и вывести их в блоке под формой, 
+после чего поля очистить.
+*/
+
+/* 
+Используя функцию drawCalendar из прошлого урока
+создать функцию drawInteractiveCalendar(el)
+Которая выводит календарь, в шапке которого отображается
+[<] месяц / год [>]
+При клике по кнопкам [<] / [>] нужно реализовать листание календаря
+Добавть на страницу index.html вызов календаря
+*/
+function drawInteractiveCalendar(el) {}
