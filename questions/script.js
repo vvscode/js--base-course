35789524c57a11ef65e5a454b97da48d850ed436
DmitryBelonohi <belonogy.dmitry@gmail.com>
2017-10-23 20:01:19 +0300

    commit

--- questions/script.js
+++ questions/script.js
@@ -1,4 +1,6 @@
-const questions = `
+/* eslint max-len: ["warn", { "ignoreStrings": true }]*/
+
+const questions1 = `
 Что такое переменная?
 как объявить переменную? как инициализировать переменную?
 виды циклов ( 3 вида ) ?
@@ -43,10 +45,48 @@ const questions = `
 Что такое пакетный менеджер? 
 Какие пакетные менеджеры есть для js?
 Что делают системы сборки? 
-Какие системы сборки есть для js?
-`
-  .trim()
-  .split('\n');
+Какие системы сборки есть для js?`;
+
+const questions2 = `Как изменить "this" внутри функции? (5 способов)
+чем различаются ".call" / ".apply" / ".bind"
+Что такое контекст вызова функции? Чем определяется?
+Что такое сигнатура функции?
+Чем характеризуется функция?
+Что такое прототип?
+Как работает конструктор? Что происходит при вызове со словом "new" ?
+Как происходит чтение свойств из объекта?
+Как происходит запись свойств в объект?
+Как проверить на принадлежность классу?
+Как работает "instanceof" ?
+4е принципа ООП
+виды полиморфизма. И их объяснение
+событийный цикл в javascript
+что такое фаза захвата / capturing ?
+что такое фаза всплытия / bubbling ?
+как подписаться на событие документа / html элемента?
+что такое "Функция высшего порядка"?
+что такое синхронный / асинхронный код?
+что такое "каррирование" ?
+в чем разница объявления методов в конструкторе и на .prototype" ?
+что такое 'полифилл'?`;
+
+const questions3 = `Что такое "стрелочная" ("arrow function") функция? Чем она отличается от обычной?
+Что такое Promise? Какой стандарт отписывает поведение Promise? Основные пункты стандарта?
+Чем куки (cookie) отличаются от localStorage ?
+Что такое CORS?
+Что такое и какие есть коды ответов HTTP?
+Что такое jsonp-запрос?
+Как код, написанный на ES6 (ES2015-2017) превратить в код, который поддерживается IE10?
+Как реализовать простейший модуль на js?
+Что такое и зачем нужен паттерн модуль? Какие модули существуют в JS?
+Как реализовать подписку на клик по кнопке, которая отработает только один раз? ( с примером кода )
+Какие события не "всплывают" ?
+Что такое делегирование?
+Преимущества и особенности работы с делегированием?
+Какие вспомогательные методы есть для работы с промисами?
+в чем разница между следующими кусками кода?<br /><pre>promise.then(onSuccess, onError);</pre><br />promise.then(onSuccess).catch(onError);
+в чем разница между следующими кусками кода?<br><pre>doSomething().then(function () { return doSomethingElse(); });</pre><br /><pre>doSomething().then(function () { doSomethingElse(); });</pre><br /><pre>doSomething().then(doSomethingElse());</pre><br /><pre>doSomething().then(doSomethingElse);</pre>
+`;
 
 const shuffleList = (list) => {
   for (let i = 0; i < list.length; i++) {
@@ -59,17 +99,46 @@ const shuffleList = (list) => {
 };
 
 const $$ = document.querySelector.bind(document);
-
-const roundQuestions = shuffleList([...questions]);
-
+let questionsStrings = [questions1, questions2, questions3];
+let roundQuestionsNumber = 0;
 let i = 0;
+
+let getQuestions = (() => {
+  let questions = [];
+  let generateQuestions = () => {
+    let level = +$$('#levelSelector').value;
+
+    questions = questionsStrings
+      .filter((_, index) => index < level)
+      .join('\n')
+      .trim()
+      .split('\n')
+      .map((i) => i.trim())
+      .filter(Boolean);
+    shuffleList([...questions]);
+    roundQuestionsNumber = questions.length;
+    i = 0;
+  };
+
+  return () => {
+    if (!questions.length) {
+      generateQuestions();
+    }
+    return questions;
+  };
+})();
+
 const drawNextQuestion = () => {
-  if (!roundQuestions.length) {
-    return;
-  }
+  let questions = getQuestions();
   i++;
-  let question = roundQuestions.pop();
-  $$('.question').innerHTML = `${i}/${questions.length} > ${question}`;
+  let question = questions.pop();
+  $$('.question').innerHTML = `<sup>${i}</sup>/<sub>${roundQuestionsNumber}</sub> > ${question}`;
 };
 
 $$('.question').addEventListener('click', drawNextQuestion);
+document.body.addEventListener('keyup', (ev) => {
+  let SPACE_KEY_CODE = 32;
+  if (ev.key === ' ' || ev.keyCode === SPACE_KEY_CODE) {
+    drawNextQuestion();
+  }
+});
