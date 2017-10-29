ef0baf71213631dadf5c1aaad82eeb9feb349372
NatalliaK <studina-natali@yandex.by>
2017-10-30 02:50:31 +0300

    Все, что пока получилось

--- 03/ht/NatalliaK/calendar.js
+++ 03/ht/NatalliaK/calendar.js
@@ -3,9 +3,18 @@ var month = new Date().getMonth();
 var year = new Date().getFullYear();
 var before;
 var after;
-
+var doc = document;
+var writeEvent = doc.createElement('div');
+var close;
+var tdAll = doc.querySelectorAll('td');
+var calendar = document.getElementById("calendar");
+var createCalendar = doc.getElementById('createCalendar');
+var selectedTd;
+var tdDay = '';
+var tdMonth = '';
+form = doc.createElement("form");
+var value = {};
 function drawCalendar(year, month, id) {
-
 
 	var date = new Date(year, month);
 
@@ -18,15 +27,15 @@ function drawCalendar(year, month, id) {
 	while (date.getMonth() === month) {
 
 		var eventDate = date.getDate();
-		//var tasks = localStorage[`event_for_` + eventDate + '/' + month] || '';
-		table += '<td data-date="' + eventDate + '" data-month="' + month + '">' + date.getDate() + /*tasks + */'</td>';
+		var tasks = localStorage[`event_for_` + eventDate + '/' + month] || '';
+		table += '<td data-date="' + eventDate + '" data-month="' + month + '">' + date.getDate() + tasks + '</td>';
 
 		if (getDay(date) % 7 === 6) {
 			table += '</tr><tr>';
 		}
 
 		date.setDate(date.getDate() + 1);
-	}
+		}
 
 	if (getDay(date) !== 0) {
 		for (i = getDay(date); i < 7; i++) {
@@ -101,11 +110,186 @@ function getNext() {
 	return inputNext;
 }
 
-	function drawInteractiveCalendar(id, year, month) {
-		year = year || data.getFullYear();
-		month = month || data.getMonth();
-		drawCalendar(year, month, "calendar");
-		writeMonthAndYear(year, month);
+function drawInteractiveCalendar(id, year, month, changeMonth, allow, allowRemove) {
+
+	year = year || data.getFullYear();
+	month = month || data.getMonth();
+	drawCalendar(year, month, id);
+	writeMonthAndYear(year, month);
+
+	if (!(changeMonth)) {
 		getPrev();
 		getNext();
 	}
+
+	if (!(allow)) {
+		calendar.ondblclick = function(event) {
+
+			var target = event.target;
+
+			if (!event.target.matches('td')) {
+				return;
+			}
+
+			if (target.tagName === "TD" && target.innerHTML !== '') {
+
+				selectTd(target);
+
+				var userEvent = prompt("Что делать будешь?", "Решать Васину задачку!");
+
+				if (userEvent === '' || userEvent === null) {
+					userEvent = 'Секретная миссия';
+				}
+
+				var userCont = doc.createElement('div');
+				userCont.className = 'userCont';
+
+				var userWr = doc.createElement('div');
+				userWr.className = 'userSelect';
+
+				userWr.innerHTML = userEvent;
+
+				target.appendChild(userCont);
+				userCont.appendChild(userWr);
+
+				if (!(allowRemove)) {
+					close = doc.createElement('button');
+					close.innerHTML = 'x';
+					userWr.appendChild(close);
+
+					close.onclick = function() {
+						var askUser = confirm('Может все-таки задачку? Точно будешь отжиматься?');
+						if (askUser) {
+							var el = this.parentNode;
+							el.parentNode.removeChild(el);
+						}
+					};
+				}
+
+				tdDay = target.getAttribute('data-date');
+				tdMonth = target.getAttribute('data-month');
+				var keyName = `event_for_` + tdDay + '/' + tdMonth || '';
+				writeEvent = localStorage.getItem(keyName);
+
+				if (localStorage[keyName] !== undefined) {
+					localStorage[keyName] += userCont.innerHTML + '<br>';
+				}
+
+				if (localStorage[keyName] === undefined) {
+					localStorage[keyName] = userCont.innerHTML + '<br>';
+				}
+			}
+		};
+	}
+}
+
+function selectTd(node) {
+	if (selectedTd) {
+		selectedTd.classList.remove('selected');
+	}
+	selectedTd = node;
+	selectedTd.classList.add('selected');
+}
+
+form.onclick = function(event) {
+
+	var target = event.target;
+
+	if (target.tagName === "INPUT") {
+
+		value.div =  (function() {
+			let id = 'calendar' + Math.random();
+			return "#" + id.replace('.', '');
+		})();
+
+		value.changeMonth = form.querySelector("input[value=changeMonth]").checked;
+
+		value.allow = form.querySelector("input[value=allow]").checked;
+
+		value.allowRemove = form.querySelector("input[value=allowRemove]").checked;
+
+		value.date = document.querySelector('input[type="month"]').value;
+	}
+
+	console.log("On form click: ", value);
+	console.log('drawInteractiveCalendar(' + value.div + ');');
+	document.querySelector('.output').innerHTML = '<div id ="' + value.div.replace('#', '') + '"></div>';
+
+	function prevDrawInteractiveCalendar(value) {
+		localStorage.clear();
+
+		var id = value.div;
+		var month = new Date(value.date).getMonth();
+		var year = new Date(value.date).getFullYear();
+		var changeMonth = value.changeMonth;
+		var allow = value.allow;
+		var allowRemove = value.allowRemove;
+		var date = value.date || null;
+
+		year = year || data.getFullYear();
+		month = month || data.getMonth();
+		drawCalendar(year, month, id);
+		writeMonthAndYear(year, month);
+
+		if (!(changeMonth === false)) {
+			getPrev();
+			getNext();
+		}
+
+		if (!(allow === false)) {
+			calendar.ondblclick = function(event) {
+
+				var target = event.target;
+
+				if (!event.target.matches('td')) {
+					return;
+				}
+
+				if (target.tagName === "TD" && target.innerHTML !== '') {
+
+					selectTd(target);
+
+					var userEvent = prompt("Что делать будешь?", "Решать Васину задачку!");
+
+					if (userEvent === '' || userEvent === null) {
+						userEvent = 'Секретная миссия';
+					}
+
+					var userWr = doc.createElement('div');
+
+					userWr.className = 'userSelect';
+
+					userWr.innerHTML = userEvent;
+
+					target.appendChild(userWr);
+
+					if (!(allowRemove)) {
+						close = doc.createElement('button');
+						close.innerHTML = 'x';
+						userWr.appendChild(close);
+
+						close.onclick = function () {
+							var askUser = confirm('Может все-таки задачку? Точно будешь отжиматься?');
+							if (askUser) {
+								var el = this.parentNode;
+								el.parentNode.removeChild(el);
+							}
+						};
+					}
+				}
+			};
+		}
+	}
+
+	prevDrawInteractiveCalendar(value);
+
+	document.querySelector('.output').innerHTML = 'drawInteractiveCalendar({<br> div: ' + value.div + ',<br>showMonth: ' + value.changeMonth + ',<br>allow: ' + value.allow + ',<br>allowRemove: ' + value.allowRemove + ',<br>date: ' + (value.date || null) + '<br>});';
+};
+
+function createForm() {
+	createCalendar.appendChild(form);
+	form.innerHTML =
+		"<fieldset><legend>Настройки календаря</legend><label><input type='checkbox' value='changeMonth'>Разрешает выбрать месяцы</label><label><input type='checkbox' value='allow'>Разрешает добавлять задания</label><label><input type='checkbox' value='allowRemove'>Разрешает удалять задания</label><input type='month' ></fieldset><div class='output'></div>";
+}
+
+createForm();
--- 03/ht/NatalliaK/configureCalendar.js
+++
@@ -1,9 +0,0 @@
-function createForm (calId){
-
-	var doc = document,
-		calId = doc.querySelector('#createCalendar'),
-		form = doc.createElement('form');
-
-		calId.appendChild(form);
-		form.innerHTML = "<fieldset><legend>Настройки календаря</legend><label><input type='checkbox' value='changeMonth'>Разрешает выбрать месяцы</label><label><input type='checkbox' value='allow'>Разрешает добавлять задания</label><label><input type='checkbox' value='allowRemove'>Разрешает удалять задания</label><input type='month' ></fieldset>";
-	}
