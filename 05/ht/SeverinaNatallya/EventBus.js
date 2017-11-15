597d0dad7f619c4704c407b34cd95435e61015b5
Natasha <natasha.awesomeyo@gmail.com>
2017-11-15 15:01:39 +0300

    05 ht done

---
+++ 05/ht/SeverinaNatallya/EventBus.js
@@ -0,0 +1,30 @@
+﻿function EventBus() {
+    this.listeners = {};
+}
+
+EventBus.prototype.trigger = function (event, data) {
+    (this.listeners[event] || []).forEach(cb => {
+        if (typeof cb === "function") {
+            cb.apply(null, Array.prototype.slice.call(arguments, 1));
+        }
+    });
+};
+EventBus.prototype.on = function (event, cb) {
+    this.listeners[event] = this.listeners[event] || [];
+    this.listeners[event].push(cb);
+};
+EventBus.prototype.off = function (event, cb) {
+    if (!cb) {
+        this.listeners[event].splice(0, this.listeners[event].length);
+        return;
+    }
+    this.listeners[event] = (this.listeners[event] || [])
+        .filter(listener => listener !== cb);
+};
+EventBus.prototype.once = function (event, cb) {
+    var item = this;
+    this.on(event, function f() {
+        cb.apply(null, arguments);
+        item.off(event, f);
+    });
+};
---
+++ 05/ht/SeverinaNatallya/Router.js
@@ -0,0 +1,52 @@
+﻿var Router = function (options) {
+    this.routes = options.routes || [];
+    this.prevRoute;
+    this.prevParam;
+    this.activeRoute;
+    this.activeParam;
+    window.addEventListener("hashchange", () =>
+        //обаботка события изменения url
+        this.handleHashChange(window.location.hash)
+    );
+    this.handleHashChange(window.location.hash); //при перезагрузке станицы вызвать нужные функции
+};
+//обработчик смены url
+Router.prototype.handleHashChange = function (url) {
+    url = url.slice(1);
+    this.routes.forEach((item, i, arr) => {
+        if (typeof item.match === "string" && item.match === url) {
+            this.activeRoute = item;
+        }
+        if (typeof item.match === "function" && item.match(url)) {
+            this.activeRoute = item;
+        }
+        if (item.match instanceof RegExp && item.match.test(url)) {
+            this.activeRoute = item;
+        }
+    });
+    this.activeParam = this.getParams(url);
+    this.execFunctions();
+};
+//извлечение параметров из url
+Router.prototype.getParams = function (url) {
+    let params = url.match(this.activeRoute.match) || [];
+    if (params.length == 2) return params[1];
+    return params;
+};
+//запуск функций из маршрутов
+Router.prototype.execFunctions = function () {
+    Promise.resolve()
+        .then(() => {
+            if (this.prevRoute) this.prevRoute.onLeave(this.prevParam);
+        })
+        .then(() => {
+            if (this.activeRoute) this.activeRoute.onBeforeEnter(this.activeParam);
+        })
+        .then(() => {
+            if (this.activeRoute) this.activeRoute.onEnter(this.activeParam);
+        })
+        .then(() => {
+            this.prevRoute = this.activeRoute;
+            this.prevParam = this.activeParam;
+        });
+};
