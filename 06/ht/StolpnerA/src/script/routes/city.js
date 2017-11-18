e02ad909f9b37f940158eadd0ab5c12fa55e38ac
AndreyS <andreysx@tut.by>
2017-11-19 02:56:47 +0300

    add xhr query

--- 06/ht/StolpnerA/src/script/routes/city.js
+++ 06/ht/StolpnerA/src/script/routes/city.js
@@ -7,12 +7,17 @@ const city = {
   match: /city=(.+)/,
   onEnter: () => {
     let country = location.hash.slice(6);
-    Promise.resolve()
-      .then(() => new Search().getCoordinates(country, "fetch"))
-      .then(
-        coords =>
-          (window.location.hash = `coordinates/latitude=${coords.lat}&longitude=${coords.lng}`)
-      );
+    let query = document.getElementsByName("typeQuery");
+    for (var i = 0; i < query.length; i++) {
+      if (query[i].checked) {
+        query = query[i].value;
+        break;
+      }
+    }
+    new Search().getCoordinates(country, query).then(coords => {
+      console.log(coords);
+      window.location.hash = `coordinates/latitude=${coords.lat}&longitude=${coords.lng}`;
+    });
   }
 };
 
