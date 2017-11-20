b76fbdb464157cdc4b8259852a41f9ba1b1b937c
Andrew1931 <andrewk1931@gmail.com>
2017-11-20 16:23:46 +0300

    ShowNameInAlert & magicProperty are done

---
+++ 02/ht/AndreiKuzmich/script.js
@@ -0,0 +1,172 @@
+/* eslint no-var: "off" */
+/* eslint no-unused-vars: "off" */
+/* eslint max-len: "off" */
+
+/**
+ * Написать функцию `isDeepEqual`
+ * которая принимает на вход двe переменных
+ * и проверяет идентичны ли они по содержимому. Например
+ * @param {*} objA 
+ * @param {*} objB 
+ * @return {boolean} идентичны ли параметры по содержимому
+ */
+function isDeepEqual(objA, objB) {
+  /* Ваше решение */
+  return undefined;
+}
+
+/**
+ * Функция фиксации контекста
+ * @param {*} func Функция для которой нужно зафиксировать контекст
+ * @param {*} context значение для this
+ * @return {function} функция с зафиксированным контекстом
+ */
+function bind(func, context) {
+  return undefined;
+}
+
+/**
+ * Реализовать метод .myBind для всех функций, 
+ * который работает так же как оригинальный .bind но не использует его внутри
+ * (можно использовать фукнцию выше)
+ */
+
+/**
+* Создать объект o так, чтобы каждый раз когда в коде написано 
+* o.magicProperty = 3 // (любое значение) 
+* в консоль выводилось значение, которое присваивается и текущее время
+*/
+
+var o = {
+   set magicProperty(value){
+     
+     var date = new Date();
+     var curretTime = date.getHours() + " часов " + date.getMinutes() + ' минут';
+     console.log('Значение, которое присваивается: ' + value + '; текущее время: ' + curretTime)
+     
+   }  
+ }
+
+/**
+* Создать конструктор с методами, так, 
+* чтобы следующий код работал и делал соответствующие вещи
+* те запуск кода ниже должен делать то, что говорят методы
+* u.askName().askAge().showAgeInConsole().showNameInAlert();
+*/
+
+function Data (name, age){
+  
+  this.askName = function(){
+    name = prompt('What is your name?');
+    return this
+  }
+  this.askAge = function(){
+    while(isNaN(age)){
+      age = prompt('How old R U?');
+    };
+    return this
+  }
+  this.showNameInAlert = function(){
+    alert ('Your name is ' + name);
+    return this
+  }
+  this.showAgeInConsole = function(){
+    console.log('Your age is ' + age)  
+  }  
+}
+
+var u = new Data()
+
+
+
+/**
+ * Написать фукнцию-калькулятор, которая работает следующим образом
+ * calculate('+')(1)(2); // 3
+ * calculate('*')(2)(3); // 6
+ * Допустимые операции : + - * /
+ */
+function calculate() {
+  /* put your code here */
+}
+
+/**
+ * Создайте конструктор-синглтон? Что такое синглтон?
+ * new Singleton() === new Singleton
+ */
+function Singleton() {
+  throw "undefined";
+}
+
+/**
+  * Создайте функцию ForceConstructor
+  * которая работает как конструктор независимо от того,
+  * вызвана она с new или без
+  * и сохраняет параметры в создаваемый объект с именами параметров
+  */
+function ForceContructor(a, b, c) {
+  throw "undefined";
+}
+
+/**
+ * Написать фукнцию сумматор, которая будет работать 
+ * var s = sum();
+ * log(s); // 0
+ * log(s(1)); // 1
+ * log(s(1)(2)); //3
+ * log(s(3)(4)(5)); // 12
+ * Число вызовов может быть неограниченым
+ */
+function sum() {
+  throw "undefined";
+}
+
+function log(x) {
+  console.log(+x);
+}
+
+/**
+ * Написать каррирующую функцию и покрыть ее тестами
+ * Функция должна поддерживать каррирование функций с 2,3,4,5 параметрами
+ * пример работы  функции
+ * 
+ * function target1(a,b,c,d) { return a + b + c + d }
+ * function target2(a,b) { return a + b }
+ * curry(target1)(1)(2)(3)(4) // 10
+ * curry(target2)(5)(8) // 13
+ * 
+ * Примеры тестов смотреть в файле тестов
+ * 
+ * Читать
+ * http://prgssr.ru/development/vvedenie-v-karrirovanie-v-javascript.html
+ * @param {*} func 
+ */
+function curry(func) {}
+
+/*
+Написать код, который для объекта созданного с помощью конструктора будет показывать, 
+что объект является экземпляром двух классов
+*/
+/* Тут ваш код */
+// User === PreUser; // false
+// u instanceof User; // true
+// u instanceof Array; // true
+// u instanceof PreUser; // true
+
+/*
+Создать веб страницу. Добавить на нее форму с полями 
+- имя (строкое поле), 
+- родной город (Выпадающий список), 
+- Комментарий (многострочное поле), пол (radiobutton). 
+При нажатии на кнопку - нужно собрать данные введенные в поля и вывести их в блоке под формой, 
+после чего поля очистить.
+*/
+
+/* 
+Используя функцию drawCalendar из прошлого урока
+создать функцию drawInteractiveCalendar(el)
+Которая выводит календарь, в шапке которого отображается
+[<] месяц / год [>]
+При клике по кнопкам [<] / [>] нужно реализовать листание календаря
+Добавть на страницу index.html вызов календаря
+*/
+function drawInteractiveCalendar(el) {}
