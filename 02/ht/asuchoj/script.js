5f1565eb63777260365b2ea12bca63753586125e
Alexej <alex.sharstyuk@gmail.com>
2017-11-30 10:03:05 +0300

    ДЗ полностью

--- 02/ht/asuchoj/script.js
+++ 02/ht/asuchoj/script.js
@@ -308,5 +308,6 @@ sleep(9);
 console.log(new Date()); // Sun Oct 08 2017 10:44:43 GMT+0300 (+03)*/
 
 function sleep(seconds){
-
+  var finish = Date.now() + seconds*1000;
+  while ( Date.now() < finish ){}
 }
