6f16ea37204455142a7a63c14ed55b1cad375b35
Sergey Yarmolenka <jibickih@gmail.com>
2017-12-04 22:52:01 +0300

    Corrected bugs

--- 03/ht/SYarmolenka/script.js
+++ 03/ht/SYarmolenka/script.js
@@ -11,85 +11,104 @@ function Calendar (htmlEl, year, month) {
 }
 
 Calendar.prototype.creatCalendar = function () {
-  let table = document.createElement(`table`); // create table
+  let table = document.createElement(`table`);
+  let self = this;
   table.className = `calendar`;
   table.id = `Cal_${this.number}`;
   table.creator = this;
   table.innerHTML = `<thead></thead><tbody><tr></tr></tbody>`; // with head and body
-
-  let thead = table.querySelector(`thead`);
-  let tr1 = document.createElement(`tr`);
-  let monthName = new Date(this.year, this.month - 1, 1, 4).toLocaleString(`ru`, {
-    month: 'long'
-  }).toUpperCase();
-  for (let i = 0; i < 3; i++) {
-    let th = document.createElement(`th`);
-    if (i === 0) {
-      th.innerHTML = '&#8656';
-      th.classList.add(`back`);
-    }
-    if (i === 1) {
-      th.setAttribute(`colspan`, `5`, 0);
-      th.innerText = `${monthName}/${this.year}`;
-    }
-    if (i === 2) {
-      th.innerHTML = '&#8658';
-      th.classList.add(`next`);
-    }
-    tr1.appendChild(th);
-  }
-  thead.appendChild(tr1);
-
-  let tr2 = document.createElement(`tr`);
-  for (let i = 0; i < 7; i++) { // write in head days of week
-    let th = document.createElement(`th`);
-    let date = new Date(2017, 10, 13 + i);
-    let op = {
-      weekday: 'short'
-    };
-    th.innerText = date.toLocaleString(`ru`, op).toUpperCase();
-    tr2.appendChild(th);
-  }
-  thead.appendChild(tr2);
-
-  let tbody = table.querySelector(`tbody`).lastElementChild;
   let count = 0;
 
-  let date = new Date(this.year, this.month - 1, 1);
-  let limit = date.getDay() > 0 ? date.getDay() : 7;
-  for (let i = 1; i < limit; i++) { // insert empty cells until first day of month
-    let td = document.createElement(`td`);
-    tbody.appendChild(td);
-    count++;
-  }
-
-  for (let i = 1; i < 32; i++) { // write days of month
-    let date = new Date(this.year, this.month - 1, i);
-    if (date.getMonth() === this.month - 1) {
-      let td = document.createElement(`td`);
-      td.innerText = date.getDate();
-      td.date = new Date(this.year, this.month - 1, td.innerText,4).toLocaleString(`en`, {
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric'
-      });
-      if (count === 7) {
-        count = 0;
-        let tr = document.createElement(`tr`);
-        table.querySelector(`tbody`).appendChild(tr);
-        tbody = table.querySelector(`tbody`).lastElementChild;
+  function createHead() {
+    let thead = table.querySelector(`thead`);
+    let tr1 = document.createElement(`tr`);
+    let monthName = new Date(self.year, self.month - 1, 1, 4).toLocaleString(`ru`, {
+      month: 'long'
+    }).toUpperCase();
+    for (let i = 0; i < 3; i++) {
+      let th = document.createElement(`th`);
+      if (i === 0) {
+        th.innerHTML = '&#8656';
+        th.classList.add(`back`);
       }
-      tbody.appendChild(td);
-      count++;
+      if (i === 1) {
+        th.setAttribute(`colspan`, `5`, 0);
+        th.innerText = `${monthName}/${self.year}`;
+      }
+      if (i === 2) {
+        th.innerHTML = '&#8658';
+        th.classList.add(`next`);
+      }
+      tr1.appendChild(th);
+    }
+    thead.appendChild(tr1);
+  }
+
+  function createDaysName() {
+    let thead = table.querySelector(`thead`);
+    let tr2 = document.createElement(`tr`);
+    for (let i = 0; i < 7; i++) { // write in head days of week
+      let th = document.createElement(`th`);
+      let date = new Date(2017, 10, 13 + i);
+      let op = {
+        weekday: 'short'
+      };
+      th.innerText = date.toLocaleString(`ru`, op).toUpperCase();
+      tr2.appendChild(th);
+    }
+    thead.appendChild(tr2);
+  }
+
+  function addEmptyCells(where) {
+    let tbody = table.querySelector(`tbody`).lastElementChild;
+    if (where === `front`) {
+      let date = new Date(self.year, self.month - 1, 1);
+      let limit = date.getDay() > 0 ? date.getDay() : 7;
+      for (let i = 1; i < limit; i++) { // insert empty cells until first day of month
+        let td = document.createElement(`td`);
+        tbody.appendChild(td);
+        count++;
+      }
+    }
+    if (where === `back`) {
+      if (tbody.querySelectorAll(`td`).length < 7) { // insert empty cells until the string end
+        for (let i = tbody.querySelectorAll(`td`).length; i < 7; i++) {
+          let td = document.createElement(`td`);
+          tbody.appendChild(td);
+        }
+      }
     }
   }
 
-  if (tbody.querySelectorAll(`td`).length < 7) { // insert empty cells until the string end
-    for (let i = tbody.querySelectorAll(`td`).length; i < 7; i++) {
-      let td = document.createElement(`td`);
-      tbody.appendChild(td);
+  function createCalendar() {
+    let tbody = table.querySelector(`tbody`).lastElementChild;
+    for (let i = 1; i < 32; i++) { // write days of month
+      let date = new Date(self.year, self.month - 1, i);
+      if (date.getMonth() === self.month - 1) {
+        let td = document.createElement(`td`);
+        td.innerText = date.getDate();
+        td.date = new Date(self.year, self.month - 1, td.innerText,4).toLocaleString(`en`, {
+          day: 'numeric',
+          month: 'numeric',
+          year: 'numeric'
+        });
+        if (count === 7) {
+          count = 0;
+          let tr = document.createElement(`tr`);
+          table.querySelector(`tbody`).appendChild(tr);
+          tbody = table.querySelector(`tbody`).lastElementChild;
+        }
+        tbody.appendChild(td);
+        count++;
+      }
     }
   }
+
+  createHead();
+  createDaysName();
+  addEmptyCells(`front`);
+  createCalendar();
+  addEmptyCells(`back`);
   return table;
 };
 
@@ -123,9 +142,9 @@ Calendar.prototype.saveChangeToLoacalHash = function() {
     jsonObj.year = this.year;
     jsonObj.month = this.month;
     jsonObj.comment = this.comment;
-    str = `Cl${this.number}&${JSON.stringify(jsonObj)}END`;
-    if (hash.indexOf(`Cl${this.number}`)>=0) {
-      if (hash.indexOf(str)>=0) return;
+    let str = `Cl${this.number}&${JSON.stringify(jsonObj)}END`;
+    if (hash.indexOf(`Cl${this.number}`) >= 0) {
+      if (hash.indexOf(str) >= 0) return;
       let first = hash.indexOf(`Cl${this.number}`);
       let last = hash.indexOf(`}END`, first)+4;
       window.location.hash = hash.slice(0, first)+str+hash.slice(last);
@@ -138,20 +157,35 @@ Calendar.prototype.addComment = function (key, value) {
   this.comment[key] = value;
 };
 
-document.body.addEventListener(`click`, function (e) {
-  if (e.target !== document.querySelectorAll(`.data`)[1]) {
-    if (document.querySelectorAll(`.data`)[1]) {
-      let comment = document.querySelectorAll(`.data`)[1];
-      if (comment.value !== ``) {
-        comment.creator.addComment(comment.saveDate, comment.value);
-        comment.creator.refreshComment();
-      }
-    }
-    while (document.querySelector(`.data`)) {
-      let elem = document.querySelector(`.data`);
-      elem.parentNode.removeChild(elem);
+function saveComment() {
+  if (document.querySelectorAll(`.data`)[1]) {
+    let comment = document.querySelectorAll(`.data`)[1];
+    if (comment.value !== ``) {
+      comment.creator.addComment(comment.saveDate, comment.value);
+      comment.creator.refreshComment();
     }
   }
+}
+function hideComment() {
+  while (document.querySelector(`.data`)) {
+    let elem = document.querySelector(`.data`);
+    elem.oninput = null;
+    elem.parentNode.removeChild(elem);
+  }
+}
+
+document.body.addEventListener(`click`, function (e) {
+  if (e.target !== document.querySelectorAll(`.data`)[1] && document.querySelector(`.data`)) {
+    saveComment();
+    hideComment();
+  }
+});
+
+document.body.addEventListener(`click`, function (e) {
+  if (!e.target.closest(`textarea,.data`)) return;
+   e.target.oninput = function(){
+     saveComment();
+   }
 });
 
 document.body.addEventListener(`click`, function (e) {
