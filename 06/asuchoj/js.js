bb34f5cfdeb738f7dcad9a81222cef93cf99f21b
Alexej <alex.sharstyuk@gmail.com>
2017-12-29 14:15:02 +0300

    1

--- 06/asuchoj/js.js
+++ 06/asuchoj/js.js
@@ -1,36 +1,32 @@
 /*обработчик для кнопки поиска*/
-var searchClick = document.querySelector('#search_click');
-var searchEnter = document.querySelector('#search_enter');
+let searchClick = document.querySelector('#search_click');
+let searchEnter = document.querySelector('#search_enter');
 
 searchClick.addEventListener('click',(e)=>{
     e.preventDefault();
-    google(searchEnter.value);
+    google(searchEnter.value)
 });
 
-/*searchEnter.addEventListener('change',(e)=>{
-    e.preventDefault();
-    google(searchEnter.value);
-});*/
-var locationCity;
-
 function google(city) {
-    var xhr = new XMLHttpRequest();
 
-    xhr.open('GET','http://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&sensor=false&language=ru',false);
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET','http://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&sensor=false&language=ru',true);
+  xhr.send();
 
-    xhr.send();
+  xhr.onreadystatechange = function () {
+
+    if (xhr.readyState !== 4) return;
+
     if (xhr.status !== 200) {
-        alert( xhr.status + ': ' + xhr.statusText );
+      alert( xhr.status + ': ' + xhr.statusText );
     }else{
-        console.log(xhr.responseText);
-        var city = JSON.parse(xhr.responseText);
-
-        locationCity = city.results[0].geometry.location;
-        console.log(locationCity.lat);
-        console.log(locationCity.lng);
+      let city = JSON.parse(xhr.responseText);
+      let locationCity = city.results[0].geometry.location
+      setCenter(locationCity.lat, locationCity.lng);
     }
+  }
+
+
 }
 
 
-
-"https://geocode-maps.yandex.ru/1.x/?geocode=Ивановка&ll=37.618920,55.756994&spn=3.552069,2.400552."
--- 06/asuchoj/mapbasics.js
+++ 06/asuchoj/mapbasics.js
@@ -1,26 +1,27 @@
-var yaMap;
-
+let yaMap;
 // Дождёмся загрузки API и готовности DOM.
+
 ymaps.ready(init);
+let info = document.querySelector('#info');
 
-var info = document.querySelector('#info');
-
-function init () {
+function init(x) {
     // Создание экземпляра карты и его привязка к контейнеру с
     // заданным id ("map").
+
     yaMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [55.76, 37.64], // Москва
-        zoom: 10,
-        controls: ['zoomControl', 'typeSelector']
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [53.90, 27.56], // Минск
+      zoom: 10,
+      controls: ['zoomControl', 'typeSelector']
     });
 
-    var showCenter = () => {
-        info.innerHTML = (info.innerHTML +'\n' + 'Center: ' + yaMap.getCenter().join(' - ')).trim();
-        yaMap.events.add('actionend', showCenter);
-        console.log(info.innerHTML);
-    };
+  var showCenter = () => info.innerHTML = (info.innerHTML +'\n' + 'Center: ' + yaMap.getCenter().join(' - ')).trim();
+  yaMap.events.add('actionend', showCenter);
 
-    showCenter();
+  showCenter();
+}
+
+function setCenter (lat, lng) {
+  yaMap.setCenter([lat, lng]);
 }
