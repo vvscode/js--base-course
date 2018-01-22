24f65cdd4558802f25b70997aae3da35a9ca02fa
Atrashevski <33287983+Atrashevski@users.noreply.github.com>
2018-01-23 01:59:44 +0300

    Update script.js

--- 01/ht/Atrashevski/script.js
+++ 01/ht/Atrashevski/script.js
@@ -21,26 +21,26 @@ function fizzBuzz() {
  * В теле функции нельзя использовать  `if`, `switch`, тернарный оператор `? :`
  */
 function fizzBuzz() {
- /* Ваше решение */
-			for(i=1;i<101;i++){
-			while(i%3==0&i%5!==0){
-				console.log("Fizz");
-				i++
-			}
-			while(i%3!==0&i%5==0){
-				console.log("Buzz");
-				i++
-			}
-			while(i%3==0&i%5==0){
-				console.log("FizzBuzz");
-				i++
-			}
-			console.log(i);
-		}
+    /* Ваше решение */
+    var i = 1;
+    for (i = 1; i < 101; i++) {
+        while (i % 3 == 0 & i % 5 !== 0) {
+            console.log("Fizz");
+            i++
+        }
+        while (i % 3 !== 0 & i % 5 == 0) {
+            console.log("Buzz");
+            i++
+        }
+        while (i % 3 == 0 & i % 5 == 0) {
+            console.log("FizzBuzz");
+            i++
+        }
+        console.log(i);
+    }
 
-		
+
 }
-
 
 /**
  * реализовать фукнцию  `isPolindrom`, 
@@ -50,21 +50,14 @@ function fizzBuzz() {
  * @return {boolean} Является строка полндромом (одинакого читается с лева на право и с права на лево ) или нет
  */
 function isPolindrom(textString) {
- /* Ваше решение */
- var arr = textString.split("");
+ 			var arr = textString.split("");
 			arr.reverse();
 			var textString2 = arr.join('');
-			var test = textString.localeCompare(textString2);
-		if(test == 0){
-			return true;
-		}
-		if (test !== 0) {
-
-			return false;
-		}
-		
- return undefined;
+			(textString == textString2)
+			return (textString == textString2) || false;
+			
 }
+
 
 
 /**
@@ -121,18 +114,16 @@ function drawCalendar(year, month, htmlEl) {
  * @return {boolean} идентичны ли параметры по содержимому
  */
 function isDeepEqual(objA, objB) {
- /* Ваше решение */
-		if (typeof objA !== typeof objB) return false;
-		if (typeof(objA) === 'object' && typeof(objB) === 'object')
-		{
-		if(objA.length !== objB.length)	return false;
-		for(var key in objA)
-		{
-		if (!isDeepEqual(objA[key], objB[key])) return false;
-		}
-		return true;
-		}
-		if (objA !== "object" || objB !== "object") return ( objA == objB );
-		
- return undefined;
+
+    if (typeof objA !== typeof objB) return false;
+    if (typeof(objA) === 'object' && typeof(objB) === 'object') {
+        if (objA.length !== objB.length) return false;
+        for (var key in objA) {
+            if (!isDeepEqual(objA[key], objB[key])) return false;
+        }
+        return true;
+    }
+    if (objA !== "object" || objB !== "object") return (objA == objB);
+
+    return undefined;
 }
