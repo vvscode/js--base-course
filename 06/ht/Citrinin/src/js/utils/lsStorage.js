30aaa762b98482e51294b3d8cb6cc598f1658d38
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-02-18 12:03:05 +0300

    Clear ht

--- 06/ht/Citrinin/src/js/utils/lsStorage.js
+++
@@ -1,6 +0,0 @@
-var lsStorage = {
-    getData: (key) => Promise.resolve(window.localStorage.getItem(key)),
-    setData: (key, value) => Promise.resolve(window.localStorage.setItem(key, value))
-};
-
-export default lsStorage;
