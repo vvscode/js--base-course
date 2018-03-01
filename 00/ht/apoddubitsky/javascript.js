6eb09907c94199c1a246fbe7382cf9448ad240a3
apoddubitsky <apoddubitsky>
2018-03-01 17:49:09 +0300

    Changes

--- 00/ht/apoddubitsky/javascript.js
+++ 00/ht/apoddubitsky/javascript.js
@@ -1,32 +1,25 @@
 "use strict";
 
-    while (true) {
-        var name = prompt("Enter your name: ", '');
-        if (name && name!='null' && isNaN(name)) {
-            break;
-        }
-    };
-    function isNumeric(n) {
-        return !isNaN(parseFloat(n))&&isFinite(n);
+while (true) {
+  var name = prompt("Enter your name: ", "");
+  if (isNaN(name) && name != "null") {
+    break;
+  }
+}
 
-    };
-
-    while (true) {
-        var age = prompt("Enter your age: ", '');
-        if (isNumeric(age)&&age>0) {
-            break;
-        }
-    };
-
+while (true) {
+  var age = prompt("Enter your age: ", "");
+  if (isFinite(age) && age > 0) {
+    break;
+  }
+}
 
 window.onload = function() {
-
-        if (age<18) {
-            return document.getElementById('txt').innerText = 'Здарова, ' + name + '. Как твои ' + age + '?';
-        }
-        else {
-            return document.getElementById('txt').innerText = 'Привествую, '+name+ '. Уж ' + age + ' лет прошло';
-        }
+  if (age < 18) {
+    return (document.getElementById("txt").innerText =
+      "Здарова, " + name + ". Как твои " + age + "?");
+  } else {
+    return (document.getElementById("txt").innerText =
+      "Привествую, " + name + ". Уж " + age + " лет прошло");
+  }
 };
-
-
