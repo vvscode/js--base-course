7065ed7bcc05cecd79a3c56b3653e7fb80d911ec
Derevyannik Sergey <33173033+dsv-sergey@users.noreply.github.com>
2018-03-03 12:26:36 +0300

    Корректировка по замечаниям
    Исправлены условия в js файле, небольшие изменения html, css

--- 00/ht/dsv-sergey/profile.js
+++ 00/ht/dsv-sergey/profile.js
@@ -1,18 +1,28 @@
 function profile() {
     let name, age, sayHallo = document.getElementById('sayHallo');
 
-    while (name == undefined || name == '' || name == ' ') {
-        name = prompt('Введите ваше имя');
+    while (name == undefined) {
+        let validName;
+        while (!validName) {
+            name = prompt('Введите ваше имя');
+            let RegExp = /^[А-Яа-яЁё]+$/;
+            validName = RegExp.exec(name);
+        }
     }
 
-    while (age == 0 || age == false || age == "" || age == null || age == undefined || age == NaN || Number(age) == NaN || age > 100) {
-        age = prompt('Введите ваш возраст')
+    while (age <= 0 || age == undefined || age > 100) {
+        let validAge;
+        while (!validAge){
+            age = prompt('Введите ваш возраст');
+            let RegExp = /^[0-9]+$/;
+            validAge = RegExp.exec(age);
+        }
     }
 
     if (age > 0 && age < 18) {
         sayHallo.innerHTML = 'Здарова, ' + name + '. Как твои ' + age + ' \?';
     } else if (age >= 18 && age < 100) {
-        sayHallo.innerHTML = 'Привествую, ' + name + '. Уж ' + age + ' лет прошло.';
+        sayHallo.innerHTML = 'Привествую, ' + '<span>' + name + '</span>' + '. Уж ' + age + ' лет прошло.';
     }
 }
 profile();
