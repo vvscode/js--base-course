3853c853681185e23d7fb9d32621ab8d0b5e63e6
dsv-sergey <dsv-sergey@mail.ru>
2018-03-04 21:15:49 +0300

    Исправление по регулярным выражениям
    Вынес из цикла регулярные выражения и переименовал, validName и validAge оставил в цикле для обнуления

--- 00/ht/dsv-sergey/profile.js
+++ 00/ht/dsv-sergey/profile.js
@@ -1,21 +1,23 @@
 function profile() {
-    let name, age, sayHallo = document.getElementById('sayHallo');
+    let name,
+        age,
+        sayHallo = document.getElementById('sayHallo'),
+        RegExpName = /^[А-Яа-яЁё]+$/,
+        RegExpAge = /^[0-9]+$/;
 
     while (name == undefined) {
         let validName;
-        let RegExp = /^[А-Яа-яЁё]+$/;
         while (!validName) {
             name = prompt('Введите ваше имя');
-            validName = RegExp.exec(name);
+            validName = RegExpName.exec(name);
         }
     }
 
     while (age <= 0 || age == undefined || age > 100) {
         let validAge;
-        let RegExp = /^[0-9]+$/;
         while (!validAge){
             age = prompt('Введите ваш возраст');
-            validAge = RegExp.exec(age);
+            validAge = RegExpAge.exec(age);
         }
     }
 
