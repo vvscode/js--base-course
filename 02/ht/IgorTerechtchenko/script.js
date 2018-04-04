6d76b2bd32e8331acf0a29a347b9666de1d1b934
IgorTerechtchenko <pokawork@gmail.com>
2018-04-04 14:40:07 +0300

    User/PreUser done

--- 02/ht/IgorTerechtchenko/script.js
+++ 02/ht/IgorTerechtchenko/script.js
@@ -6,37 +6,37 @@
  * Написать функцию `isDeepEqual`
  * которая принимает на вход двe переменных
  * и проверяет идентичны ли они по содержимому. Например
- * @param {*} objA 
- * @param {*} objB 
+ * @param {*} objA
+ * @param {*} objB
  * @return {boolean} идентичны ли параметры по содержимому
  */
 
 function isDeepEqual(objA, objB) {
-  var argsPair = '' + arguments[0] + arguments[1]
-  if (argsPair in isDeepEqual) {
-    isDeepEqual[argsPair] += 1
+  var argsPair = '' + arguments[0] + arguments[1];
+  if (isDeepEqual[argsPair]) {
+    isDeepEqual[argsPair] += 1;
   } else {
-    isDeepEqual[argsPair] = 1
+    isDeepEqual[argsPair] = 1;
   }
-  if (objA !== objA && objB !== objB) {
-    return true
+  if (objA !== objA && objB !== objB) { //comparing NaN
+    return true;
   }
   if (objA.length !== objB.length) {
     return false;
   }
 
   if (typeof objA !== 'object') {
-    return (objA === objB)
+    return objA === objB;
   }
 
   if (Object.keys(objA).length !== Object.keys(objB).length) {
     return false;
   }
-  
+
   for (var i in objA) {
     if (isDeepEqual[argsPair] > 1) {
-      isDeepEqual[argsPair] = 0
-      break
+      isDeepEqual[argsPair] = 0;
+      break;
     }
     if (!isDeepEqual(objA[i], objB[i])) {
       return false;
@@ -52,61 +52,62 @@ function isDeepEqual(objA, objB) {
  * @return {function} функция с зафиксированным контекстом
  */
 function bind(func, context) {
-  if (context) {
-    console.log(context)
-    context.f = func
-    return context.f
-  } else {
-    return func
-  }
+  return function() {
+    return func.apply(context, arguments);
+  };
 }
 
 /**
- * Реализовать метод .myBind для всех функций, 
+ * Реализовать метод .myBind для всех функций,
  * который работает так же как оригинальный .bind но не использует его внутри
  * (можно использовать фукнцию выше)
  */
 
+Function.prototype.myBind = function(context) {
+  return bind(this, context);
+};
 /**
-* создать объект с волшебным свойством, 
-* чтобы при присвоении ему значения, в консоль выводилась текущая дата и значение, которое присваиваем. 
-* А при чтении всегда выводилось число на 1 больше предыдущего
-* o.magicProperty = 5; // 'Sat Mar 24 2018 13:48:47 GMT+0300 (+03) -- 5'
-* console.log(o.magicProperty); // 6
-* console.log(o.magicProperty); // 7
-* console.log(o.magicProperty); // 8
-*/
+ * создать объект с волшебным свойством,
+ * чтобы при присвоении ему значения, в консоль выводилась текущая дата и значение, которое присваиваем.
+ * А при чтении всегда выводилось число на 1 больше предыдущего
+ * o.magicProperty = 5; // 'Sat Mar 24 2018 13:48:47 GMT+0300 (+03) -- 5'
+ * console.log(o.magicProperty); // 6
+ * console.log(o.magicProperty); // 7
+ * console.log(o.magicProperty); // 8
+ */
 
 /**
-* Создать конструктор с методами, так, 
-* чтобы следующий код работал и делал соответствующие вещи
-* те запуск кода ниже должен делать то, что говорят методы
-* u.askName().askAge().showAgeInConsole().showNameInAlert();
-*/
+ * Создать конструктор с методами, так,
+ * чтобы следующий код работал и делал соответствующие вещи
+ * те запуск кода ниже должен делать то, что говорят методы
+ * u.askName().askAge().showAgeInConsole().showNameInAlert();
+ */
 function User() {
-  var age
-  var name
+  var age;
+  var name;
 
-  this.askName = function() {
-    this.name = window.prompt('enter your name'); 
-    return this
-  }
-
-  this.askAge = function() {
-    this.age = window.prompt('enter your age');
-    return this
-  }
-
-  this.showAgeInConsole = function() {
-    console.log(this.age)
-    return this
-  }
-
-  this.showNameInAlert = function() {
-    window.alert(this.name)
-    return this
-  }
 }
+
+User.prototype.askName = function() {
+    this.name = window.prompt('enter your name');
+    return this;
+};
+
+User.prototype.askAge = function() {
+  this.age = window.prompt('enter your age');
+  return this;
+};
+
+User.prototype.showAgeInConsole = function() {
+  console.log(this.age);
+  return this;
+};
+
+User.prototype.showNameInAlert = function() {
+  window.alert(this.name);
+  return this;
+};
+
 /**
  * Написать фукнцию-калькулятор, которая работает следующим образом
  * calculate('+')(1)(2); // 3
@@ -114,14 +115,14 @@ function User() {
  * Допустимые операции : + - * /
  */
 function calculate(sign) {
-  this.sign = sign
-  var arg1
+  this.sign = sign;
+  var arg1;
   return function(arg) {
-    this.arg1 = arg 
+    this.arg1 = arg;
     return function(arg) {
-      return eval(this.arg1 + this.sign + arg)
-    }
-  }
+      return eval(this.arg1 + this.sign + arg);
+    };
+  };
 }
 
 /**
@@ -131,30 +132,29 @@ function calculate(sign) {
 
 function Singleton() {
   if (Singleton.instance) {
-    return Singleton.instance
-  } else { 
-    Singleton.instance = this
+    return Singleton.instance;
+  } else {
+    Singleton.instance = this;
   }
 }
 
 /**
-  * Создайте функцию ForceConstructor
-  * которая работает как конструктор независимо от того,
-  * вызвана она с new или без
-  * и сохраняет параметры в создаваемый объект с именами параметров
-  */
+ * Создайте функцию ForceConstructor
+ * которая работает как конструктор независимо от того,
+ * вызвана она с new или без
+ * и сохраняет параметры в создаваемый объект с именами параметров
+ */
 function ForceContructor(a, b, c) {
   if (!new.target) {
-    //why doesn't work without return?
-    return new ForceContructor(a, b, c) 
+    return new ForceContructor(a, b, c);
   }
-  this.a = a
-  this.b = b
-  this.c = c
+  this.a = a;
+  this.b = b;
+  this.c = c;
 }
 
 /**
- * Написать фукнцию сумматор, которая будет работать 
+ * Написать фукнцию сумматор, которая будет работать
  * var s = sum();
  * log(s); // 0
  * log(s(1)); // 1
@@ -163,6 +163,7 @@ function ForceContructor(a, b, c) {
  * Число вызовов может быть неограниченым
  */
 function sum(arg) {
+  return function(sum) {} 
 }
 
 function log(x) {
@@ -173,17 +174,17 @@ function log(x) {
  * Написать каррирующую функцию и покрыть ее тестами
  * Функция должна поддерживать каррирование функций с 2,3,4,5 параметрами
  * пример работы  функции
- * 
+ *
  * function target1(a,b,c,d) { return a + b + c + d }
  * function target2(a,b) { return a + b }
  * curry(target1)(1)(2)(3)(4) // 10
  * curry(target2)(5)(8) // 13
- * 
+ *
  * Примеры тестов смотреть в файле тестов
- * 
+ *
  * Читать
  * http://prgssr.ru/development/vvedenie-v-karrirovanie-v-javascript.html
- * @param {*} func 
+ * @param {*} func
  */
 function curry(func) {}
 
@@ -191,12 +192,14 @@ function curry(func) {}
 Написать код, который для объекта созданного с помощью конструктора будет показывать, 
 что объект является экземпляром двух классов
 */
-/* Тут ваш код */
 // User === PreUser; // false
 // u instanceof User; // true
 // u instanceof Array; // true
 // u instanceof PreUser; // true
 
+function PreUser() {}
+PreUser.prototype = Object.create(Array.prototype)
+User.prototype = Object.create(PreUser.prototype)
 /*
 Создать веб страницу. Добавить на нее форму с полями 
 - имя (строкое поле), 
