3b9fdd5f362421ca168bb0325ebac3b5a59ee0b0
dsv-sergey <dsv-sergey@mail.ru>
2018-04-23 01:42:29 +0300

    Внесено часть исправлений, добавлен getCounter без тестов

--- 02/ht/dsv-sergey/script.js
+++ 02/ht/dsv-sergey/script.js
@@ -6,40 +6,48 @@
  * Написать функцию `isDeepEqual`
  * которая принимает на вход двe переменных
  * и проверяет идентичны ли они по содержимому. Например
- * @param {*} objA 
- * @param {*} objB 
+ * @param {*} objA
+ * @param {*} objB
  * @return {boolean} идентичны ли параметры по содержимому
  */
 function isDeepEqual(objA, objB) {
-    if (typeof objA === 'string' && typeof objB === 'string') {
-        return objA === objB;
-    }
-    if (Array.isArray(objA) && Array.isArray(objB)) {
-        return objA.toString() === objB.toString();
-    }
-    if (typeof objA === 'number' && typeof objB === 'number' && !Number.isNaN(objA)) {
-        return objA === objB;
-    }
-    if (typeof objA === 'number' && typeof objB === 'number' && Number.isNaN(objA) === Number.isNaN(objB)) {
-        return true;
-    }
-    if (typeof objA === 'object' && typeof objB === 'object') {
-        if (JSON.stringify(objA) === JSON.stringify(objB)) {
-            return true;
-        } else {
-            for (var i in objA) {
-                if (!(i in objB) || !isDeepEqual(objA[i], objB[i])) {
-                    return false;
-                }
-            }
-        }
-        for (var i in objA) {
-            if (objA.hasOwnProperty(i) !== objB.hasOwnProperty(i)) {
-                return false;
-            }
-        }
-    }
+  if (typeof objA === "string" && typeof objB === "string") {
+    return objA === objB;
+  }
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return objA.toString() === objB.toString();
+  }
+  if (
+    typeof objA === "number" &&
+    typeof objB === "number" &&
+    !Number.isNaN(objA)
+  ) {
+    return objA === objB;
+  }
+  if (
+    typeof objA === "number" &&
+    typeof objB === "number" &&
+    Number.isNaN(objA) === Number.isNaN(objB)
+  ) {
     return true;
+  }
+  if (typeof objA === "object" && typeof objB === "object") {
+    if (JSON.stringify(objA) === JSON.stringify(objB)) {
+      return true;
+    } else {
+      for (var i in objA) {
+        if (!(i in objB) || !isDeepEqual(objA[i], objB[i])) {
+          return false;
+        }
+      }
+    }
+    for (var i in objA) {
+      if (objA.hasOwnProperty(i) !== objB.hasOwnProperty(i)) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
@@ -50,37 +58,38 @@ function isDeepEqual(objA, objB) {
  */
 function bind(func, context) {
   return function() {
-      return func.apply(context, arguments);
+    return func.apply(context, arguments);
   };
 }
 
 /**
- * Реализовать метод .myBind для всех функций, 
+ * Реализовать метод .myBind для всех функций,
  * который работает так же как оригинальный .bind но не использует его внутри
  * (можно использовать фукнцию выше)
  */
 
 Function.prototype.myBind = function(context) {
-    return bind(this, context);
+  return bind(this, context);
 };
 
 /**
-* Создать объект o так, чтобы каждый раз когда в коде написано 
-* o.magicProperty = 3 // (любое значение) 
-* в консоль выводилось значение, которое присваивается и текущее время
-*/
+ * Создать объект o так, чтобы каждый раз когда в коде написано
+ * o.magicProperty = 3 // (любое значение)
+ * в консоль выводилось значение, которое присваивается и текущее время
+ */
 
 function magicProperty() {
-    return {
-        set x(value) {
-            count = value;
-            return console.log(new Date() + ' -- ' + value);
-        },
-        get x() {
-            count++;
-            return count;
-        }
-    };
+  var count;
+  return {
+    set x(value) {
+      count = value;
+      return console.log(new Date() + " -- " + value);
+    },
+    get x() {
+      count++;
+      return count;
+    }
+  };
 }
 
 var o = magicProperty();
@@ -90,31 +99,40 @@ console.log(o.x);
 console.log(o.x);
 
 /**
-* Создать конструктор с методами, так, 
-* чтобы следующий код работал и делал соответствующие вещи
-* те запуск кода ниже должен делать то, что говорят методы
-* u.askName().askAge().showAgeInConsole().showNameInAlert();
-*/
+ * Создать конструктор с методами, так,
+ * чтобы следующий код работал и делал соответствующие вещи
+ * те запуск кода ниже должен делать то, что говорят методы
+ * u.askName().askAge().showAgeInConsole().showNameInAlert();
+ */
 
-var us = {
+function UserBot() {
+  return {
     askName: function() {
-        this.name = prompt('Введите имя');
-        return this;
+      this.name = prompt("Введите имя");
+      return this;
     },
     askAge: function() {
-        this.age = prompt('Введите возраст');
-        return this;
+      this.age = prompt("Введите возраст");
+      return this;
     },
     showAgeInConsole: function() {
-        console.log('Ваш возраст: ' + this.age);
-        return this;
+      console.log("Ваш возраст: " + this.age);
+      return this;
     },
     showNameInAlert: function() {
-        alert('Вас зовут ' + this.name);
-        return this;
+      alert("Вас зовут " + this.name);
+      return this;
     },
-};
-us.askName().askAge().showAgeInConsole().showNameInAlert();
+  };
+}
+
+var us = new UserBot();
+
+us
+  .askName()
+  .askAge()
+  .showAgeInConsole()
+  .showNameInAlert();
 
 /**
  * Написать фукнцию-калькулятор, которая работает следующим образом
@@ -125,36 +143,36 @@ us.askName().askAge().showAgeInConsole().showNameInAlert();
 
 function calculate(operation) {
   var solution = 0;
-    switch (operation) {
-      case '+':
-        return function(a) {
-          return function(b) {
-              solution = a + b;
-              return solution;
-          };
+  switch (operation) {
+    case "+":
+      return function(a) {
+        return function(b) {
+          solution = a + b;
+          return solution;
         };
-    case '-':
-        return function(a) {
-            return function(b) {
-                solution = a - b;
-                return solution;
-            };
+      };
+    case "-":
+      return function(a) {
+        return function(b) {
+          solution = a - b;
+          return solution;
         };
-    case '*':
-        return function(a) {
-            return function(b) {
-                solution = a * b;
-                return solution;
-            };
+      };
+    case "*":
+      return function(a) {
+        return function(b) {
+          solution = a * b;
+          return solution;
         };
-    case '/':
-        return function(a) {
-            return function(b) {
-                solution = a / b;
-                return solution;
-            };
+      };
+    case "/":
+      return function(a) {
+        return function(b) {
+          solution = a / b;
+          return solution;
         };
-    }
+      };
+  }
 }
 
 /**
@@ -162,34 +180,34 @@ function calculate(operation) {
  * new Singleton() === new Singleton
  */
 var Singleton = new function() {
-    var instance;
-    function Singleton() {
-        if (!instance) {
-            instance = this;
-        }
-        return instance;
+  var instance;
+  function Singleton() {
+    if (!instance) {
+      instance = this;
     }
-    return Singleton;
-};
+    return instance;
+  }
+  return Singleton;
+}();
 
 /**
-  * Создайте функцию ForceConstructor
-  * которая работает как конструктор независимо от того,
-  * вызвана она с new или без
-  * и сохраняет параметры в создаваемый объект с именами параметров
-  */
+ * Создайте функцию ForceConstructor
+ * которая работает как конструктор независимо от того,
+ * вызвана она с new или без
+ * и сохраняет параметры в создаваемый объект с именами параметров
+ */
 function ForceContructor(a, b, c) {
-    if (this instanceof ForceContructor) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    } else {
-        return new ForceContructor(a, b, c);
-    }
+  if (this instanceof ForceContructor) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  } else {
+    return new ForceContructor(a, b, c);
+  }
 }
 
 /**
- * Написать фукнцию сумматор, которая будет работать 
+ * Написать фукнцию сумматор, которая будет работать
  * var s = sum();
  * log(s); // 0
  * log(s(1)); // 1
@@ -198,56 +216,56 @@ function ForceContructor(a, b, c) {
  * Число вызовов может быть неограниченым
  */
 function sum(a) {
-    var currentSum;
-    if (a === undefined) {
-        a = 0;
+  var currentSum;
+  if (a === undefined) {
+    a = 0;
+  }
+  currentSum = +a;
+  function sumFunc(b) {
+    if (b === undefined) {
+      b = 0;
     }
-    currentSum =+ a;
-    function sumFunc(b) {
-        if (b === undefined) {
-            b = 0;
-            }
-        return sum(currentSum + b);
-    }
-    sumFunc.valueOf = function() {
-        return currentSum;
-    };
-    return sumFunc;
+    return sum(currentSum + b);
+  }
+  sumFunc.valueOf = function() {
+    return currentSum;
+  };
+  return sumFunc;
 }
 
 /**
  * Написать каррирующую функцию и покрыть ее тестами
  * Функция должна поддерживать каррирование функций с 2,3,4,5 параметрами
  * пример работы  функции
- * 
+ *
  * function target1(a,b,c,d) { return a + b + c + d }
  * function target2(a,b) { return a + b }
  * curry(target1)(1)(2)(3)(4) // 10
  * curry(target2)(5)(8) // 13
- * 
+ *
  * Примеры тестов смотреть в файле тестов
- * 
+ *
  * Читать
  * http://prgssr.ru/development/vvedenie-v-karrirovanie-v-javascript.html
- * @param {*} func 
+ * @param {*} func
  */
 function curry(func) {
-    var arr = [],
-        counter = func.length;
-    return function fn() {
-        if (arguments.length > 1) {
-            for (var n = 0; n < arguments.length; n++) {
-                arr = arr.concat(arguments[n]);
-            }
-        } else {
-            arr.push(arguments[0]);
-        }
-        if (counter == arr.length) {
-            return func.apply(null, arr);
-        } else {
-            return fn;
-        }
-    };
+  var arr = [],
+    counter = func.length;
+  return function fn() {
+    if (arguments.length > 1) {
+      for (var n = 0; n < arguments.length; n++) {
+        arr = arr.concat(arguments[n]);
+      }
+    } else {
+      arr.push(arguments[0]);
+    }
+    if (counter == arr.length) {
+      return func(...arr);
+    } else {
+      return fn;
+    }
+  };
 }
 
 /*
@@ -283,26 +301,23 @@ User.prototype = Object.create(PreUser.prototype);
 */
 function drawInteractiveCalendar(el) {}
 
-
 /* создать функцию, которая не может работать как конструктор (работать с `new`, и покрыть ее тестами
 NotContructor
 
  */
 function NotContructor() {
-    if (this instanceof NotContructor) {
-        throw new TypeError('"NotContructor" is not a constructor.');
-    }
+  if (this instanceof NotContructor) {
+    throw new TypeError('"NotContructor" is not a constructor.');
+  }
 }
 /*
     Написать реализацию метода `.myCall`, который будет работать аналогично системному `.call` и
 покрыть реализацию тестами
 */
 
-function myCall(context, ) {
-    this.context = context;
-    return {
-
-    }
+function myCall(context) {
+  this.context = context;
+  return {};
 }
 
 /*
@@ -314,19 +329,19 @@ function myCall(context, ) {
  */
 
 function throttle(fn, time) {
-    var throttled = false,
-        args,
-        context;
-    function wrapper() {
-        if (throttled) {
-            args = arguments;
-            context = this;
-            return;
-        }
-        fn.apply(this,arguments);
-
-        setTimeout(wrapper,time)
+  var throttled = false,
+    args,
+    context;
+  function wrapper() {
+    if (throttled) {
+      args = arguments;
+      context = this;
+      return;
     }
+    fn.apply(this, arguments);
+
+    setTimeout(wrapper, time);
+  }
 }
 
 /*
@@ -339,7 +354,7 @@ function throttle(fn, time) {
    (https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage).
    Интерфейс работы с данными (чтение/запись) лучше сделать асинхронным
  */
-//Calendar.js
+// calendar.js
 /*
 Создать синхронную функцию `sleep(seconds)` так, чтобы работал код
 
@@ -351,7 +366,7 @@ console.log(new Date()); // Sun Oct 08 2017 10:44:43 GMT+0300 (+03)
  */
 
 function sleep(seconds) {
-    return;
+  return;
 }
 
 /*
@@ -372,13 +387,23 @@ c
 ```
  */
 
-function getCounter() {
+var getCounter = function(arr) {
+    var counter = 0;
+    if (arr != 'undefined') {
+        counter = arr;
+    };
     return {
-        log: function () {
-
+        log: function() {
+            console.log(counter);
+            return this;
         },
-        add: function () {
-
-        };
-    }
-}
+        add: function(a) {
+            counter += a;
+            return this;
+        },
+        reset: function() {
+            counter =0;
+            return this;
+        },
+    };
+};
