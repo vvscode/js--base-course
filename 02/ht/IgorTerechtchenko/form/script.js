dc59499b8231a79f01a90447b9d70d682085b026
IgorTerechtchenko <pokawork@gmail.com>
2018-04-25 14:19:28 +0300

    form edited

--- 02/ht/IgorTerechtchenko/form/script.js
+++ 02/ht/IgorTerechtchenko/form/script.js
@@ -1,29 +1,21 @@
-var input = document.getElementsByTagName('input');
-var button = document.getElementById('button');
-var age = document.getElementsByTagName('select');
-var bio = document.getElementsByTagName('textarea');
-var userInfoArea = document.getElementById('out');
-console.log(button)
-
-function User() {
-  this.name = input[0].value;
-  this.gender = input[1].checked ? 'male' : 'female';
-  this.age = age[0].value;
-  this.drivingLicense = input[3].checked ? 'yes' : 'no';
-  this.bio = bio[0].value;
+function getUser() {
+  var input = document.getElementsByTagName('input');
+  var button = document.getElementById('button');
+  var age = document.getElementsByTagName('select');
+  var bio = document.getElementsByTagName('textarea');
+  var form = document.getElementById('form');
+  return { 'name' : input[0].value,
+           'gender': input[1].checked ? 'male' : 'female',
+           'age': age[0].value,
+           'drivinglicense': input[3].checked ? 'yes' : 'no',
+           'bio': bio[0].value
+  }
 }
-
-button.addEventListener('click', displayUser, false);
 
 function displayUser(el) {
   el.preventDefault();
-  var user = new User();
-  for(var field in user){
-    if(!user[field]) {
-      window.alert('fill in all fields');
-      return
-    }
-  }
+  var userInfoArea = document.getElementById('out');
+  var user = getUser();
   var text = '<div> name: ' + user.name + ' <br>' +
       'gender: ' + user.gender + ' <br>' + 
       'age: ' + user.age + '<br>' +
@@ -32,3 +24,5 @@ function displayUser(el) {
       '</div>';
   userInfoArea.innerHTML = text;
 }
+
+form.addEventListener('submit', displayUser, false);
