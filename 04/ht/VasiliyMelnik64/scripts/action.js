f235285091a696467b1dc30163e2983796eaef1f
VasiliyMelnik <vasiliymelnik64@gmail.com>
2018-05-03 16:42:57 +0300

    remove method renderTasks from localStorage to actions

--- 04/ht/VasiliyMelnik64/scripts/action.js
+++ 04/ht/VasiliyMelnik64/scripts/action.js
@@ -55,5 +55,37 @@ var actions = {
     args.forEach(function(el) {
       el.classList.add("g-hide");
     });
+  },
+  /**
+   * @function
+   * @param {elem} - element with calendar
+   * @param {currentMonth} - database name of the key with array of tasks in this month
+   * @var {allChildren} - array with rows of cells of the table (trs)
+   *
+   * renders tasks on Calendar
+   */
+  renderTasks: function (elem, currentMonth) { 
+    var allChildren = [].slice.call(elem.children[0].children[0].children);
+    for (var i = 2; i < allChildren.length; i++) {
+      var line = allChildren[i].cells;
+
+      for (var j = 0; j < line.length; j++) {
+        var dateNumber = line[j].firstElementChild;
+
+        if (
+          !dateNumber ||
+          !dateNumber.innerHTML ||
+          !currentMonth[dateNumber.innerHTML]
+        ) {
+          continue;
+        }
+        var div = line[j].getElementsByTagName("div")[0];
+        var dateContent = currentMonth[dateNumber.innerHTML];
+
+        for (var k = 0; k < dateContent.length; k++) {
+          div.innerHTML += dateContent[k];
+        }
+      }
+    }
   }
 };
