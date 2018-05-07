e8cf422318d0a342d7ed8e3fb7d8549d0afad8e5
VasiliyMelnik <vasiliymelnik64@gmail.com>
2018-05-07 17:03:51 +0300

    add tests for navigation menu and the interaction of boxes and the block with the object of settings

--- 04/ht/VasiliyMelnik64/tests/calendar.test.js
+++ 04/ht/VasiliyMelnik64/tests/calendar.test.js
@@ -1,32 +1,54 @@
 "@fixture calendar";
 "@page https://vasiliymelnik64.github.io/projects/interactiveCalendar/index.html#Calendar_1";
 
-"@test"["headerNavAsserts"] = {
-    "1.Assert": function() {
-        eq($("[name='Calendar_0'].item__link").length > 0, true);
-        eq($(":containsExcludeChildren(Calendar)").eq(0).length > 0, true);
-        eq($(".list__item.item").find(" > a:nth(0)").eq(0).length > 0, true);
-        eq($("body > div:nth(1) > header:nth(0) > ul:nth(0) > li:nth(0) > a:nth(0)").length > 0, true);
-        ok($("[name='Calendar_0'].item__link").length > 0);
-        ok($(":containsExcludeChildren(Calendar)").eq(0).length > 0);
-        ok($(".list__item.item").find(" > a:nth(0)").eq(0).length > 0);
-        ok($("body > div:nth(1) > header:nth(0) > ul:nth(0) > li:nth(0) > a:nth(0)").length > 0);
-        eq($(":containsExcludeChildren(Create)").length > 0, true);
-        eq($("[name='Calendar_1'].item__link").length > 0, true);
-        eq($(".list__item.item").find(" > a:nth(0)").eq(1).length > 0, true);
-        eq($("body > div:nth(1) > header:nth(0) > ul:nth(0) > li:nth(1) > a:nth(0)").length > 0, true);
-        ok($(":containsExcludeChildren(Create)").length > 0);
-        ok($("[name='Calendar_1'].item__link").length > 0);
-        ok($(".list__item.item").find(" > a:nth(0)").eq(1).length > 0);
-        ok($("body > div:nth(1) > header:nth(0) > ul:nth(0) > li:nth(1) > a:nth(0)").length > 0);
-        eq($(":containsExcludeChildren(About)").length > 0, true);
-        eq($("[name='About'].item__link").length > 0, true);
-        eq($(".list__item.item").find(" > a:nth(0)").eq(2).length > 0, true);
-        eq($("body > div:nth(1) > header:nth(0) > ul:nth(0) > li:nth(2) > a:nth(0)").length > 0, true);
-        ok($(":containsExcludeChildren(About)").length > 0);
-        ok($("[name='About'].item__link").length > 0);
-        ok($(".list__item.item").find(" > a:nth(0)").eq(2).length > 0);
-        ok($("body > div:nth(1) > header:nth(0) > ul:nth(0) > li:nth(2) > a:nth(0)").length > 0);
+
+"@test"["testing navigation"] = {
+    '1.Click link "Calendar"': function() {
+        act.click("[name='Calendar_0'].item__link");
+    },
+    "2.Assert": function() {
+        ok(getComputedStyle(document.querySelector("#about")).display == 'none');
+    },
+    '3.Click link "Create"': function() {
+        act.click("[name='Calendar_1'].item__link");
+    },
+    "4.Assert": function() {
+        ok(getComputedStyle(document.querySelector("#code")).display != 'none');
+        ok(getComputedStyle(document.querySelector("fieldset")).display != 'none');
+    },
+    '5.Click link "About"': function() {
+        act.click("[name='About'].item__link");
+    },
+    "6.Assert": function() {
+        ok(getComputedStyle(document.querySelector("#about")).display != 'none');
+        ok(getComputedStyle(document.querySelector("#code")).display == 'none');
+        ok(getComputedStyle(document.querySelector("fieldset")).display == 'none');
     }
 };
 
+"@test"["testing form"] = {
+    '1.Change the checkbox "Allow change month"': function () {
+        act.click("[data-change='_allowChange']");
+    },
+    "2.Assert": function() {
+        ok(~document.getElementsByTagName('pre')[0].innerHTML.indexOf('allowChange : true;'));
+    },
+    '3.Change the checkbox "Allow add tasks"': function () {
+        act.click("[data-change='_allowAdd']");
+    },
+    "4.Assert": function() {
+        ok(~document.getElementsByTagName('pre')[0].innerHTML.indexOf('allowAdd : true;'));
+    },
+    '5.Change the checkbox "Allow remove tasks"': function () {
+        act.click("[data-change='_allowRemove']");
+    },
+    "6.Assert": function() {
+        ok(~document.getElementsByTagName('pre')[0].innerHTML.indexOf('allowRemove : true;'));
+    },
+    '7.Change the checkbox "Show month / year"': function () {
+        act.click("[data-change='_showMonth']");
+    },
+    "8.Assert": function() {
+        ok(~document.getElementsByTagName('pre')[0].innerHTML.indexOf('showMonth : true;'));
+    }
+};
