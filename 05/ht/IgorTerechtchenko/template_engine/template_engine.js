e67355c269912446f4e55434625769e7edde0532
IgorTerechtchenko <pokawork@gmail.com>
2018-06-08 14:16:38 +0300

    more efficient regex + removed debug output

--- 05/ht/IgorTerechtchenko/template_engine/template_engine.js
+++ 05/ht/IgorTerechtchenko/template_engine/template_engine.js
@@ -5,25 +5,10 @@ function compileTemplate(template) {
       var tmp; 
       if(/{{.+}}/g.test(word)) {
         tmp = word.replace(/{{/g, '').replace(/}}/g, ''); 
-        template[template.indexOf(word)] = data[tmp];
+        template[template.indexOf(word)] = template[template.indexOf(word)]
       }
     el.innerHTML = template.join(' ');
     });
   }
   return output; 
 }
-
-var template = '{{name}} is {{age}} years old'
-var element = document.createElement('div');
-var dataObj = { name: 'Bob', age: 33 }; 
-compileTemplate(template)(element, dataObj);
-
-//compileTemplate = (tpl) => (el, data)
-//Компиляция шаблона возвращает функцию
-//при передаче этой функции элемента и объекта с данными
-//элемент получает разметку в соответсвии с tpl из data
-//var tpl = "{{name}} is {{age}} years old";
-//var el = document.compileElement('div');
-//var template = compileTemplate(tpl);
-//template(el, { name: 'Bob', age: 33 });
-//el.innerHTML; // 'Bob is 33 years old'
