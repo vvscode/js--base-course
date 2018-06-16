82a6a8dce18e7ac9ee724fb2570457f7c5248a28
dsv-sergey <dsv-sergey@mail.ru>
2018-06-16 09:55:47 +0300

    Add route

--- 05/ht/dsv-sergey/script.js
+++ 05/ht/dsv-sergey/script.js
@@ -41,4 +41,58 @@ EventBus.prototype.once = function(event, hendler) {
     this.on(event, wrapper);
 };
 
+function HashRouter() {
+	this.handleUrl( getHash() );
+    window.addEventListener("hashchange", function() { this.handleUrl( getHash() ) }.bind(this)
+    );
+}
 
+HashRouter.prototype = {
+    handleUrl: function(url) {
+        var routes = this.routes || [];
+        console.log(routes);
+        var result = findRoute(routes, url);
+        var route = result[0];
+        var params = result[1];
+        if (!route) {
+            return;
+        }
+
+        Promise.resolve()
+            .then(() => {
+                if (route.onBeforeEnter) {
+                    return route.onBeforeEnter.call(route, params);
+                }
+            })
+            .then(() => {
+                if (route.onEnter) {
+                    route.onEnter.call(route, params);
+                }
+            });
+    }
+};
+
+function getHash() {
+    return decodeURI(window.location.hash).slice(1);
+};
+
+function findRoute(routeList, url) {
+    var result = [null, null];
+    routeList.forEach(function(route) {
+        if (result[0]) {
+            return;
+        } // если роут уже нашли - остальное не проверяем
+        if (route.match === url) {
+            result = [route, url];
+        } else if (RegExp(route.match).test(url)) {
+            result = [route, url.match(route.match)];
+        } else if (
+            typeof route.match === "function" &&
+            route.match.call(this, url)
+        ) {
+            result = [route, route.match.call(this, url)];
+            console.log(result);
+        }
+    });
+    return result;
+}
--- 05/ht/dsv-sergey/test.js
+++ 05/ht/dsv-sergey/test.js
@@ -153,6 +153,179 @@ describe("EventBus", function() {
     });
 });
 
-describe("Router", () => {
-    it("is it function", () => assert.equal(typeof Router, "function"));
+describe("HashRouter", () => {
+    it("is function and constructor", () => {
+        assert.equal(typeof HashRouter, "function");
+        assert.isOk(new HashRouter() instanceof HashRouter);
+    });
+
+    describe(".handleUrl", () => {
+        it("is method (on prototype)", () => {
+            assert.equal(typeof new HashRouter().handleUrl, "function");
+            assert.equal(
+                new HashRouter().handleUrl,
+                HashRouter.prototype.handleUrl
+            );
+        });
+
+        it("called on start with current hash", () => {
+            window.location.hash = "some string";
+            var handleUrl = HashRouter.prototype.handleUrl;
+            let param = null;
+            HashRouter.prototype.handleUrl = url => (param = url);
+            new HashRouter();
+            HashRouter.prototype.handleUrl = handleUrl;
+            assert.equal(param, "some string");
+        });
+
+        it("called on hashchange with current hash", done => {
+            window.location.hash = "some string";
+            var handleUrl = HashRouter.prototype.handleUrl;
+            let param = null;
+            HashRouter.prototype.handleUrl = url => (param = url);
+            new HashRouter();
+            window.location.hash = "some new string";
+            setTimeout(() => {
+                HashRouter.prototype.handleUrl = handleUrl;
+                done(assert.equal(param, "some new string"));
+            }, 150);
+        });
+
+        it("calls .onBeforeEnter on matched route from `this.routes` with param", done => {
+            var x = 0;
+            var param = null;
+            var o = {
+                routes: [
+                    {
+                        match: "string",
+                        onBeforeEnter: url => {
+                            x++;
+                            param = url;
+                        }
+                    }
+                ]
+            };
+            window.location.hash = "string";
+            HashRouter.prototype.handleUrl.call(o, "string");
+            setTimeout(() => {
+                assert.equal(x, 1);
+                assert.equal(param, "string");
+                done();
+            }, 100);
+        });
+        it("calls .onEnter on matched route from `this.routes` with param", done => {
+            var x = 0;
+            var param = null;
+            var o = {
+                routes: [
+                    {
+                        match: "string",
+                        onEnter: url => {
+                            x++;
+                            param = url;
+                        }
+                    }
+                ]
+            };
+            window.location.hash = "string";
+            HashRouter.prototype.handleUrl.call(o, "string");
+            setTimeout(() => {
+                assert.equal(x, 1);
+                assert.equal(param, "string");
+                done();
+            }, 100);
+        });
+        it("calls .onEnter on matched route from `this.routes` with param", done => {
+            var x = [];
+            var o = {
+                routes: [
+                    {
+                        match: "string",
+                        onBeforeEnter: () => {
+                            x.push("onBeforeEnterStart");
+                            return new Promise(resolve => {
+                                setTimeout(() => {
+                                    x.push("onBeforeEnterEnd");
+                                    resolve();
+                                }, 100);
+                            });
+                        },
+                        onEnter: () => {
+                            x.push("onEnterStart");
+                            return new Promise(resolve => {
+                                setTimeout(() => {
+                                    x.push("onEnterEnd");
+                                    resolve();
+                                }, 100);
+                            });
+                        }
+                    }
+                ]
+            };
+            window.location.hash = "string";
+            HashRouter.prototype.handleUrl.call(o, "string");
+            setTimeout(() => {
+                console.log(x);
+                assert.deepEqual(x, [
+                    "onBeforeEnterStart",
+                    "onBeforeEnterEnd",
+                    "onEnterStart",
+                    "onEnterEnd"
+                ]);
+                done();
+            }, 400);
+        });
+    });
+
+    describe("findRoute", () => {
+        var flag = false;
+        var funcParam;
+        var str = { match: "string" };
+        var regExp = { match: /param=(.+)/ };
+        var func = {
+            match: url => {
+                funcParam = url;
+                return flag ? [1, flag, url] : null;
+            }
+        };
+        it("is function", () => {
+            assert.equal(typeof findRoute, "function");
+        });
+        it("takes 2 params", () => assert.equal(findRoute.length, 2));
+        it("returns list of nulls by default", () => {
+            assert.deepEqual(findRoute([], "some_url"), [null, null]);
+        });
+        it("able to find route from list by string match", () => {
+            assert.deepEqual(findRoute([regExp, str, func], "string"), [
+                str,
+                "string"
+            ]);
+        });
+        it("able to find route from list by regExp", () => {
+            assert.deepEqual(findRoute([regExp, str, func], "param=123"), [
+                regExp,
+                ["param=123", "123"]
+            ]);
+        });
+        it("able to find route from list by functional match", () => {
+            flag = false;
+            assert.deepEqual(findRoute([regExp, str, func], "xxxx"), [
+                null,
+                null
+            ]);
+            flag = true; // now func returns list / truethly value
+            assert.deepEqual(findRoute([regExp, str, func], "xxxxyy"), [
+                func,
+                [1, flag, "xxxxyy"]
+            ]);
+            assert.equal(funcParam, "xxxxyy", "passes url into function");
+        });
+        it("returns first match", () => {
+            flag = true;
+            assert.deepEqual(findRoute([regExp, str, func], "string"), [
+                str,
+                "string"
+            ]);
+        });
+    });
 });
