7844c494dbe190ef10057e2db83b69e752181063
IgorTerechtchenko <pokawork@gmail.com>
2018-06-24 17:53:04 +0300

    folder reorganization, optimised template, gitignore fix

---
+++ 05/ht/IgorTerechtchenko/event_bus.js
@@ -0,0 +1,27 @@
+function EventBus() {
+  
+}
+
+EventBus.prototype.on = function(eventName, cb) {
+  if(!this[eventName]) {
+    this[eventName] = []
+  }
+  this[eventName].push(cb);
+}
+EventBus.prototype.off = function(eventName, cb) {
+  this[eventName] = this[eventName].filter(function(func) {
+    return func !== cb; 
+  });
+}
+EventBus.prototype.trigger = function(eventName, arg) {
+  if(this[eventName]) {
+    this[eventName].forEach(function(cb) {cb(arg)});
+  }
+}
+EventBus.prototype.once = function(eventName, cb) {
+  var wrapper = function(arg) {
+    this.off(eventName, wrapper);
+    return cb(arg); 
+  }.bind(this);
+  this.on(eventName, wrapper);
+}
