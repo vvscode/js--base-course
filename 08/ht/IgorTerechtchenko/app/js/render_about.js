751d313c82264c75f171296bacb74d4f664a50a6
IgorTerechtchenko <pokawork@gmail.com>
2018-07-21 16:21:33 +0300

    added about page

--- 08/ht/IgorTerechtchenko/app/js/render_about.js
+++ 08/ht/IgorTerechtchenko/app/js/render_about.js
@@ -1,2 +1,12 @@
-function renderAbout(el) {
+export default function renderAbout(el) {
+  var aboutWrapper = document.createElement('div');
+  var p = document.createElement('p1');
+  aboutWrapper.className = 'aboutWrapper';
+  p.innerHTML = 'life game SPA by Igor Terechtchenko, 2018';
+  aboutWrapper.appendChild(p);
+  var divs = el.querySelectorAll('div');
+  divs.forEach((div) => {
+    div.innerHTML = '';
+  });
+  el.appendChild(aboutWrapper);
 }
