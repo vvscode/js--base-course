13ee8a59d90c934d62acb6d3e490b02b6cfb9917
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-10-17 21:58:23 +0400

    Reset progress

--- 10/ht/anna-kiranova/todo-list/src/actions/filter.js
+++
@@ -1,9 +0,0 @@
-import { FILTER_UPDATE } from '../constants/index';
-
-export const filterUpdate = (name, value) => ({
-    type: FILTER_UPDATE,
-    payload: {
-        value,
-        name
-    }
-})
