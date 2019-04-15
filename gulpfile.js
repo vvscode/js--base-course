8aae44cee818979f640d0e5a4954cf4a82658e20
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-04-15 22:43:01 +0400

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,21 @@ const st = require('st');
 const FILES_TO_WATCH_FOR = ['./**/*.*', '!node_modules/**/*.*'];
 
 gulp.task('watch', ['server'], () =>
-  gulp.watch(FILES_TO_WATCH_FOR, e => {
+  gulp.watch(FILES_TO_WATCH_FOR, (e) => {
     console.log(e);
     gulp.src(e.path || FILES_TO_WATCH_FOR).pipe(livereload());
-  }),
+  })
 );
 
-gulp.task('server', done => {
+gulp.task('server', (done) => {
   http
-    .createServer(
-      st({
-        path: `${__dirname}/`,
-        index: true,
-        cache: false,
-      }),
-    )
-    .listen(8080, done);
+      .createServer(
+          st({
+            path: `${__dirname}/`,
+            index: true,
+            cache: false,
+          })
+      )
+      .listen(8080, done);
   livereload.listen();
 });
