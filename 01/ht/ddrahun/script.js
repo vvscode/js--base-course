0663e2bd4e0a0013a14fed4fa77ea8c002a743f2
DashaDrahun <daryawater@gmail.com>
2019-07-25 12:58:45 +0300

    refactoring
    refactored methods

--- 01/ht/ddrahun/script.js
+++ 01/ht/ddrahun/script.js
@@ -9,9 +9,6 @@
 function log(a) {
     console.log(a);
 }
-
-/* Раместите ваш код ниже */
-
 /**
  * реализовать фукнцию `fizzBuzz` 
  * которая выводит числа от 1 до 100. 
@@ -22,27 +19,14 @@ function log(a) {
  * В теле функции нельзя использовать  `if`, `switch`, тернарный оператор `? :`
  */
 function fizzBuzz() {
-    function GetDividersAndIsItDivided(dividend, ...dividers) {
-        var dividersAndIsItDivided = {};
-        for (let i of dividers) {
-            dividersAndIsItDivided[i] = dividend % i == 0;
-        }
-
-        var allDividers = dividers.join('');
-        var isDividedByAllDividers = Object.values(dividersAndIsItDivided).indexOf(false) === -1;
-        dividersAndIsItDivided[allDividers] = isDividedByAllDividers;
-
-        return dividersAndIsItDivided;
-    }
-
     var printer = {
-        3: function () {
+        "3": function () {
             log(`Fizz`);
         },
-        5: function () {
+        "5": function () {
             log(`Buzz`)
         },
-        35: function () {
+        "35": function () {
             log(`FizzBuzz`)
         },
         "undefined": function (number) {
@@ -51,10 +35,24 @@ function fizzBuzz() {
     }
 
     for (var i = 1; i <= 100; i++) {
-        var dividers = GetDividersAndIsItDivided(i, 3, 5);
+        var dividers = getDividersAndIsItDivided(i, 3, 5);
         var indexOfDivider = Object.values(dividers).lastIndexOf(true);
         var divider = Object.keys(dividers)[indexOfDivider];
         printer[divider](i);
+    }
+
+    function getDividersAndIsItDivided(dividend, ...dividers) {
+        var dividersAndIsItDivided = {};
+
+        for (let i of dividers) {
+            dividersAndIsItDivided[i] = dividend % i === 0;
+        }
+
+        var allDividers = dividers.join('');
+        var isDividedByAllDividers = Object.values(dividersAndIsItDivided).indexOf(false) === -1;
+        dividersAndIsItDivided[allDividers] = isDividedByAllDividers;
+
+        return dividersAndIsItDivided;
     }
 }
 /**
@@ -68,10 +66,9 @@ function isPolindrom(textString) {
     var strArray = Array.from(textString.toLowerCase());
     var reversedArray = strArray.reverse();
     var reversedString = reversedArray.join('');
+
     return textString == reversedString;
 }
-
-
 /**
  * Реализовать фукнцию `drawCalendar` , 
  * которая принимает три аргумента - год, месяц, htmlElement 
@@ -81,45 +78,6 @@ function isPolindrom(textString) {
  * @param {external:HTMLElement} htmlEl 
  */
 function drawCalendar(year, month, htmlEl) {
-
-    function GetLastDayOfMonth(year, month) {
-        for (var i = 28; i <= 31; i++) {
-            if (new Date(year, month, i).getMonth() > month)
-                return i - 1;
-        }
-        return i - 1;
-    }
-
-    function GetCalendarTable(year, jsMonth, lastDateNumber) {
-        var table = document.createElement('table');
-        table.setAttribute("border", 1)
-        var header = table.appendChild(document.createElement('tr'));
-        for (var key in daysOfWeek) {
-            var th = document.createElement('th');
-            th.textContent = daysOfWeek[key];
-            header.appendChild(th);
-        }
-        var rowsNumber = GetNumberOfRows(year, jsMonth, lastDateNumber)
-        for (var i = 0; i < rowsNumber; i++) {
-            table.appendChild(document.createElement('tr'));
-        }
-        return table;
-    }
-
-    function GetNumberOfRows(year, jsMonth, lastDateNumber) {
-        var firstDayOfWeek = new Date(year, jsMonth, 1)
-        var positionInWeek = firstDayOfWeek.getDay();
-        var allCells = lastDateNumber + positionInWeek;
-        var allRows = allCells / 7;
-        return (allCells % 7 > 0) ? allRows + 1 : allRows;
-    }
-
-    function AddTd(row, text) {
-        var td = document.createElement('td');
-        td.innerHTML = text;
-        row.appendChild(td);
-    }
-
     var daysOfWeek = {
         0: "ВС",
         1: "ПН",
@@ -130,8 +88,8 @@ function drawCalendar(year, month, htmlEl) {
         6: "СБ"
     }
     var jsMonth = month - 1;
-    var lastDayNumber = GetLastDayOfMonth(year, jsMonth);
-    var table = GetCalendarTable(year, jsMonth, lastDayNumber);
+    var lastDayNumber = getLastDayOfMonth(year, jsMonth);
+    var table = getCalendarTable(year, jsMonth, lastDayNumber);
     var rowsNumber = table.getElementsByTagName('tr').length;
     var firstDayOfWeek = new Date(year, jsMonth, 1).getDay();
 
@@ -141,18 +99,58 @@ function drawCalendar(year, month, htmlEl) {
         for (var cell = 0; cell < Object.keys(daysOfWeek).length; cell++) {
             if (day > lastDayNumber) break;
             if (cell < firstDayOfWeek && day == 1) {
-                AddTd(row, "");
-            }
-            else {
-                AddTd(row, day);
+                addTd(row, "");
+            } else {
+                addTd(row, day);
                 day++;
             }
         }
     }
+
     htmlEl.innerHTML = table;
+
+    function getLastDayOfMonth(year, month) {
+        for (var i = 28; i <= 31; i++) {
+            if (new Date(year, month, i).getMonth() > month)
+                return i - 1;
+        }
+        return i - 1;
+    }
+
+    function getCalendarTable(year, jsMonth, lastDateNumber) {
+        var table = document.createElement('table');
+        table.setAttribute("border", 1);
+        var header = table.appendChild(document.createElement('tr'));
+        var rowsNumber = getNumberOfRows(year, jsMonth, lastDateNumber);
+
+        for (var key in daysOfWeek) {
+            var th = document.createElement('th');
+            th.textContent = daysOfWeek[key];
+            header.appendChild(th);
+        }
+
+        for (var i = 0; i < rowsNumber; i++) {
+            table.appendChild(document.createElement('tr'));
+        }
+
+        return table;
+
+        function getNumberOfRows(year, jsMonth, lastDateNumber) {
+            var firstDayOfWeek = new Date(year, jsMonth, 1)
+            var positionInWeek = firstDayOfWeek.getDay();
+            var allCells = lastDateNumber + positionInWeek;
+            var allRows = allCells / 7;
+
+            return (allCells % 7 > 0) ? allRows + 1 : allRows;
+        }
+    }
+
+    function addTd(row, text) {
+        var td = document.createElement('td');
+        td.innerHTML = text;
+        row.appendChild(td);
+    }
 }
-
-
 /**
  * Написать функцию `isDeepEqual`
  * которая принимает на вход двe переменных
@@ -162,35 +160,32 @@ function drawCalendar(year, month, htmlEl) {
  * @return {boolean} идентичны ли параметры по содержимому
  */
 function isDeepEqual(objA, objB) {
+    return (!areArrays(objA, objB) && !areObjects(objA, objB)) ? objA == objB : areObjectsEqual(objA, objB);
 
-    function AreObjects(objA, objB) {
+    function areObjects(objA, objB) {
         return typeof (objA) == 'object' && typeof (objB) == 'object' && !Array.isArray(objA) && !Array.isArray(objB);
     }
 
-    function AreArrays(objA, objB) {
+    function areArrays(objA, objB) {
         return Array.isArray(objA) && Array.isArray(objB);
     }
 
-    function AreObjectsEqual(objA, objB) {
+    function areObjectsEqual(objA, objB) {
         if (Object.keys(objA).length !== Object.keys(objB).length)
             return false;
         for (var key in objA) {
             if (key in objB) {
-                if (AreObjects(objA[key], objB[key]) || AreArrays(objA[key], objB[key])) {
-                    AreObjectsEqual(objA[key], objB[key]);
-                }
-                else {
-                    if (objA[key] == objB[key])
+                if (areObjects(objA[key], objB[key]) || areArrays(objA[key], objB[key])) {
+                    areObjectsEqual(objA[key], objB[key]);
+                } else {
+                    if (objA[key] === objB[key])
                         continue;
                     else return false;
                 }
-            }
-            else {
+            } else {
                 return false;
             }
         }
         return true;
     }
-
-    return (!AreArrays(objA, objB) && !AreObjects(objA, objB)) ? objA == objB : AreObjectsEqual(objA, objB);
 }
