dfb88c97d09873fc682be6b5c5d4907a5b8d244a
Vasiliy Vanchuk <v.vanchuk@tut.by>
2020-02-28 15:55:35 +0200

    Create apply.test.js

---
+++ .ht/diff/apply.test.js
@@ -0,0 +1,34 @@
+mocha.setup({
+  ui: 'bdd',
+  bail: true,
+});
+const assert = chai.assert;
+
+describe("Реализовать фукнцию `apply` тремя способами", () => {
+  [
+    { title: 'Определение функции', name: 'applyFD' },
+    { title: 'Выражение функции', name: 'applyFE' },
+    { title: 'Конструирование функции', name: 'applyFС' },
+  ].forEach((testItem) => {
+     describe(`${itemTitle} (${item.name})`, () => {
+         it(`функция ${item.name} определена`, () => {
+             assert.equal(typeof window[item.name], 'function');
+         });
+         
+         it(`вызывает функцию с аргументами`, () => {
+            let a;
+            let fun = (a, b) => a = [a, b];
+            window[item.name](fun, 1, 2);
+            assert.equal(Array.isArray(a), true);
+            assert.equal(a[0], 1);
+            assert.equal(a[1], 2);
+         });
+         
+        it(`возвращает результат работы функции`, () => {
+            let ret = Math.random();
+            let fun = (a, b) => ret;
+            assert.equal(window[item.name](fun, 1, 2), ret);
+         });
+     });
+  });
+});
